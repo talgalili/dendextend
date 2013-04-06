8a55439abf7288468d529c71df2a9138d66b638c
Tal Galili <tal.galili@gmail.com>
2013-04-06 08:22:50 +0300

    Merged "labels<-" help files
    Proper use of s3method documentation in roxygen2
    
    This now passes all CHECKS.
    e.g: the code is CRAN-ready level   :)

--- R/labels.r
+++ R/labels.r
@@ -4,18 +4,38 @@
 
 
 
+
+
 # turning label insertion function into a method:
 
 #' @title "label" assignment operator
 #' @description "label" assignment operator for vectors, dendrogram, and hclust classes.
-#' @aliases "labels<-.default" "labels<-.dendrogram"
+#' @aliases 
+#' labels<-.default 
+#' labels<-.dendrogram 
+#' labels.hclust labels<-.hclust
+#' labels.matrix labels<-.matrix
+#' @usage
+#' labels(object, ...) <- value
+#' 
+#' \method{labels}{matrix}(object, which = c("colnames", "rownames"), ...)
+#' 
+#' \method{labels}{matrix}(object, which = c("colnames", "rownames"), ...) <- value
+#' 
+#' \method{labels}{dendrogram}(object, ...) <- value
+#' 
+#' \method{labels}{hclust}(object, ...)
+#' 
+#' \method{labels}{hclust}(object, ...) <- value
 #' @param object a variable name (possibly quoted) who's label are to be updated
+#' @param which "colnames" or "rownames", to which of the two should labels refer to.
+#' @param ... parameters passed (not currently in use)
 #' @param value a value to be assigned to object's label
 #' @return The updated object
 #' @author Gavin Simpson, Tal Galili
 #' @export
 #' @source The functions here are based on code by Gavin and kohske from (adopted to dendrogram by Tal Galili): \url{http://stackoverflow.com/questions/4614223/how-to-have-the-following-work-labelsx-some-value-r-question}
-#' @seealso \code{\link{labels}}
+#' @seealso \code{\link{labels}}, \code{\link{labels.matrix}}
 #' @examples
 #' x <- 1:3 
 #' labels(x)
@@ -29,6 +49,21 @@
 #' # get("labels<-")
 #' 
 #' ################
+#' # Example for matrix objects
+#' 
+#' x <- matrix(1:9, 3,3)
+#' labels(x)
+#' x
+#' labels(x) <- letters[1:3]
+#' x
+#' labels(x, which = "rownames") <- LETTERS[24:26]
+#' x
+#' #  a b c
+#' #X 1 4 7
+#' #Y 2 5 8
+#' #Z 3 6 9
+#' 
+#' ################
 #' # Example for using the assignment with dendrogram and hclust objects:
 #' hc <- hclust(dist(USArrests[1:3,]), "ave")
 #' dend <- as.dendrogram(hc)
@@ -39,14 +74,15 @@
 #' labels(dend) # "Arizona" "Alabama" "Alaska" 
 #' labels(dend) <- letters[1:3]
 #' labels(dend) # "a" "b" "c"
-"labels<-" <- function (object,value, ...) UseMethod("labels<-")
+"labels<-" <- function(object,..., value) UseMethod("labels<-")
 
 # example("labels<-")
 # ?"labels<-"
 
 
+# ' @title "label" assignment operator - default
 #' @export
-"labels<-.default" <- function (object, ..., value) {
+"labels<-.default" <- function(object,..., value) {
    # object <- value		
    # since labels first goes to object names, then a change in objects labels (in a default setting), should result in changing it's "names"
    # x <- 1:3
@@ -57,18 +93,6 @@
    object
 }
 
-
-# getAnywhere("labels.default")
-# getAnywhere("names<-")
-# x <- 1:4
-# labels(x) <- 2:5
-# x
-# str(x)
-# labels(x)
-# traceback()
-# x <- 1:4
-# labels(x) <- 2:5
-# labels(x)[2:3] <- 9:10
 
 
 
@@ -107,9 +131,10 @@
 # }
 
 
-
-#' @export
-"labels<-.dendrogram" <- function (object,..., value) {
+# ' @title "label" assignment operator - dendrogram
+# ' @export
+#' @S3method labels<- dendrogram
+"labels<-.dendrogram" <- function(object,..., value) {
    # credit for the help on how to write this type of function goes to:
    # Gavin Simpson and also kohske, see here:
    # http://stackoverflow.com/questions/4614223/how-to-have-the-following-work-labelsx-some-value-r-question
@@ -145,12 +170,15 @@
 }
 
 
-
-#' @export
+# ' @title Find Labels from hclust Object
+# ' @export
+#' @S3method labels hclust
 labels.hclust <- function(object, ...)  as.character(object$labels)
 
-#' @export
-"labels<-.hclust" <- function (object,..., value) {
+# ' @title "label" assignment operator - hclust
+# ' @export
+#' @S3method labels<- hclust
+"labels<-.hclust" <- function(object,..., value) {
    # this function gets:
    #	object - a hclust object
    # 	value - the values of the new labels to insert instead of the old labels	
@@ -163,52 +191,38 @@ labels.hclust <- function(object, ...)  as.character(object$labels)
 
 
 
-
-#' @title "label" assignment operator for matrix class
-#' @aliases 'labels<-.matrix'
-#' @param object a variable name (possibly quoted) who's label are to be updated
-#' @param which "colnames" or "rownames", to which of the two should labels refer to.
-#' @param value a value to be assigned to object's label
-#' @return The matrix object with updated col/row names
-#' @author Gavin Simpson, Tal Galili
-#' @export
-#' @source Based on code by Gavin Simpson from: \url{http://stackoverflow.com/questions/4614223/how-to-have-the-following-work-labelsx-some-value-r-question}
-#' @seealso \code{\link{labels}}
-#' @examples
-#' x <- matrix(1:9, 3,3)
-#' labels(x)
-#' x
-#' labels(x) <- letters[1:3]
-#' x
-#' labels(x, which = "rownames") <- LETTERS[24:26]
-#' x
-#' #  a b c
-#' #X 1 4 7
-#' #Y 2 5 8
-#' #Z 3 6 9
-labels.matrix <- function(obj, which = c("colnames","rownames"), ...) {
+# ' @title "label" assignment operator for matrix class
+# ' @export
+#' @S3method labels matrix
+labels.matrix <- function(object, which = c("colnames","rownames"), ...) {
    if(missing(which))
       which <- "colnames"
    which <- match.arg(which)
    if(which == "colnames") {
-      out <- colnames(obj)
+      out <- colnames(object)
    } else {
-      out <- rownames(obj)
+      out <- rownames(object)
    }
    out
 }
 # example("labels.matrix")
+# ?"labels.matrix"
 
-
-#' @export
-'labels<-.matrix' <- function(obj, which = c("colnames","rownames"), ..., value) {
+# ' @title "label" assignment operator - matrix
+# ' @export
+#' @S3method labels<- matrix
+'labels<-.matrix' <- function(object, which = c("colnames","rownames"), ..., value) {
    if(missing(which))
       which <- "colnames"
    which <- match.arg(which)
    if(which == "colnames") {
-      colnames(obj) <- value
+      colnames(object) <- value
    } else {
-      rownames(obj) <- value
+      rownames(object) <- value
    }
-   obj
+   object
 }
+
+
+# methods("labels")
+# methods("labels<-")
