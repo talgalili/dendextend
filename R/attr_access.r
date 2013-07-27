5d6414e01b6cd17c61762d7e5aa068aa831aebdd
Tal Galili <tal.galili@gmail.com>
2013-07-27 18:31:52 +0300

    Some work on attr_access.r (not sure if this would be used later or not...).

---
+++ R/attr_access.r
@@ -0,0 +1,43 @@
+## I'm not sure if I'll use it yet. But just in case...
+#######
+#######
+
+
+#' @title Get/set attributes of dendrogram's leaves
+### #' @aliases 
+### #' set_leaves_attr
+### #' @usage
+### #' get_leaves_attr(object, labels = TRUE,...)
+### #' 
+### #' set_leaves_attr(object, ...) <- value
+#' @export
+#' @param object a dendrogram object 
+#' @param attribute character scaler of the attribute (\code{attr})
+#' we wish to get/set from the leaves
+#' @param ... not used
+#' @source Heavily inspired by the code in the 
+#' function \code{labels.dendrogram}, 
+#' so credit should go to Martin Maechler.
+#' @return 
+#' A vector with the dendrogram's leaves attribute
+#' @examples
+#' # define dendrogram object to play with:
+#' hc <- hclust(dist(USArrests[1:3,]), "ave")
+#' dend <- as.dendrogram(hc)
+#' 
+#' # get_leaves_attr(dend) # error :)
+#' get_leaves_attr(dend, "label")
+#' labels(dend, "label")
+#' get_leaves_attr(dend, "height") # should be 0's
+#' get_leaves_attr(dend, "leaf") # should be TRUE's
+#' get_leaves_attr(dend, "members") # should be TRUE's
+get_leaves_attr <- function (object, attribute, ...) {
+   if(!inherits(object,'dendrogram')) warning("'object' should be a dendrogram.")   
+   if(missing(attribute)) stop("'attribute' parameter is missing.")
+   
+   get_attr_from_leaf <- function(dend_node) {
+      if(is.leaf(dend_node)) attr(dend_node, attribute)
+   }   
+   
+   return(unlist(dendrapply(object, get_attr_from_leaf)))   
+}
