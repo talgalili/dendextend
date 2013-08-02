d467113521d3c7584e45b7ac30fc11352fa7b521
Tal Galili <tal.galili@gmail.com>
2013-08-02 20:42:32 +0300

    Renaming the file: unroot.r -> unbranch.r

--- R/unbranch.r
+++ R/unbranch.r
@@ -190,7 +190,15 @@ unbranch.hclust <- function(x, branch_becoming_root = 1, new_root_height, ...) {
 
 
 #' @S3method unbranch phylo
-unbranch.phylo <- function(x, ...) ape:::unbranch(phy = x)
+unbranch.phylo <- function(x, ...) {
+		# ape:::unbranch(phy = x)
+   x_dend <- as.dendrogram(x)
+   x_dend_unbranch <- unbranch(x_dend, branch_becoming_root , new_root_height, ...)
+   x_unbranch <- as.phylo(x_dend_unbranch)  
+
+   return(x_unbranch)
+}
+
 
 
 
