35cf12da241892ec951a3a7507263cef0e34f707
Tal Galili <tal.galili@gmail.com>
2013-08-18 12:31:58 +0300

    FM_index_profdpm function added.

--- R/bk_method.r
+++ R/bk_method.r
@@ -17,43 +17,225 @@
 #
 
 
-###
-#
-# Bk-method.r
-
-# require(R2PPT)
 
 
+#' @title Calculating Fowlkes-Mallows index using the profdpm R package
+#' @export
+#' @description
+#' 
+#' Calculating Fowlkes-Mallows index using the \code{pci} function from
+#' the profdpm R package. This function uses very fast C code (thanks to
+#' Matthew Shotwell's work).
+#' 
+#' As opposed to the \code{\link{FM_index_R}} function, the \code{FM_index_profdpm}
+#' function does NOT calculate the Expectancy or the variance of the FM Index
+#' under the null hypothesis of no relation.
+#' 
+#' 
+#' @param A1_clusters a numeric vector of cluster grouping (numeric) of items,
+#' with a name attribute of item name for each element from group A1.
+#' These are often obtained by using some k cut on a dendrogram.
+#' @param A2_clusters a numeric vector of cluster grouping (numeric) of items,
+#' with a name attribute of item name for each element from group A2.
+#' These are often obtained by using some k cut on a dendrogram.
+#' @param assume_sorted_vectors logical (FALSE). Can we assume to two group 
+#' vectors are sorter so that they have the same order of items?
+#' IF FALSE (default), then the vectors will be sorted based on their
+#' name attribute.
+#' @param warn logical (TRUE). Should a warning be issued in case of problems?
+#' @param ... Ignored.
+#' 
+#' @details
+#' From Wikipedia:
+#' 
+#' Fowlkes-Mallows index (see references) is an external evaluation method 
+#' that is used to determine the similarity between two clusterings
+#' (clusters obtained after a clustering algorithm). This measure of similarity
+#' could be either between two hierarchical clusterings or a clustering and
+#' a benchmark classification. A higher the value for the Fowlkes-Mallows index
+#' indicates a greater similarity between the clusters and the benchmark 
+#' classifications.
+#' 
+#' @seealso
+#' \link{cor_bakers_gamma}
+#' @return 
+#' The Fowlkes-Mallows index between two vectors of clustering groups.
+#' 
+#' @references
+#' 
+#' Fowlkes, E. B.; Mallows, C. L. (1 September 1983).
+#' "A Method for Comparing Two Hierarchical Clusterings".
+#' Journal of the American Statistical Association 78 (383): 553.
+#' 
+#' Shotwell, Matthew S. "profdpm: An R Package for MAP Estimation in a Class
+#' of Conjugate Product Partition Models."
+#' Journal of Statistical Software 53: 1-18.
+#' 
+#' \url{http://en.wikipedia.org/wiki/Fowlkes-Mallows_index}
+#' 
+#' @examples
+#' 
+#' \dontrun{
+#' 
+#' set.seed(23235)
+#' ss <- TRUE # sample(1:150, 10 )
+#' hc1 <- hclust(dist(iris[ss,-5]), "com")
+#' hc2 <- hclust(dist(iris[ss,-5]), "single")
+#' # dend1 <- as.dendrogram(hc1)
+#' # dend2 <- as.dendrogram(hc2)
+#' #    cutree(dend1)   
+#' 
+#' FM_index_profdpm(cutree(hc1, k=3), cutree(hc1, k=3)) # 1
+#' set.seed(1341)
+#' FM_index_profdpm(cutree(hc1, k=3), sample(cutree(hc1, k=3)), assume_sorted_vectors =TRUE) # 0.38037
+#' FM_index_profdpm(cutree(hc1, k=3), sample(cutree(hc1, k=3)), assume_sorted_vectors =FALSE) # 1 again :)
+#' FM_index_profdpm(cutree(hc1, k=3), cutree(hc2, k=3)) # 0.8059
+#' FM_index_profdpm(cutree(hc1, k=30), cutree(hc2, k=30)) # 0.4529
+#' 
+#' fo <- function(k) FM_index_profdpm(cutree(hc1, k), cutree(hc2, k)) 
+#' lapply(1:4, fo)
+#' ks <- 1:150
+#' plot(sapply(ks, fo)~ ks, type = "b", main = "Bk plot for the iris dataset")
+#' 
+#' }
+FM_index_profdpm <- function(A1_clusters, A2_clusters, assume_sorted_vectors =FALSE, warn = TRUE, ...) {
+   if(!require("profdpm")) {
+      if(warn) warning("The 'profdpm' package is not installed. Reverting to using the 'FM_index_R' function.")
+      return(FM_index_R(A1_clusters, A2_clusters,assume_sorted_vectors = assume_sorted_vectors, warn= warn,...))
+   }
+   # see page 9 and 10, here:
+   # http://ftp.daum.net/CRAN/web/packages/profdpm/vignettes/profdpm.pdf
+   
+   # sanity checks in case the names of the vectors do not make sense:
+   if(!assume_sorted_vectors) {      
+      names_A1_clusters <- names(A1_clusters)
+      names_A2_clusters <- names(A2_clusters)
+      
+      if(is.null(names_A1_clusters) || is.null(names_A2_clusters)) {
+         if(warn) warning("Names of the clusters are NULL, we will assume the vectors are sorted.")
+         assume_sorted_vectors <- TRUE
+      }
+      if(length(names_A1_clusters) != length(names_A2_clusters)) {
+         if(warn) warning("Names of the clusters are not of equal length, we will assume the vectors are sorted.")
+         assume_sorted_vectors <- TRUE
+      }
+      if(length(names_A1_clusters) != length(A1_clusters)) {
+         if(warn) warning("Names of the clusters are not of equal length as that of the clusters, we will assume the vectors are sorted.")
+         assume_sorted_vectors <- TRUE
+      }
+   }         
+   # if we are still NOT assuming the vectors are sorted - we can sort them...
+   if(!assume_sorted_vectors) {
+      A1_clusters <- A1_clusters[order(names_A1_clusters)]   # order the vec accourding to the names, so to allow a comparison
+      A2_clusters <- A2_clusters[order(names_A2_clusters)]   # order the vec accourding to the names, so to allow a comparison
+   }
 
-##example:
-# mydata <- USArrests
-##Ward Hierarchical Clustering
-# d <- dist(mydata, method = "euclidean") # distance matrix
-# fit1 <- hclust(d, method="ward") 
-# fit2 <- hclust(d, method="single") 
-##fit <- fit1
-# par(mfrow = c(1,2))
-# plot(fit1) # display dendogram
-# plot(fit2) # display dendogram
-
-if(F) {
-   system.time(Bk(dendo1, dendo2)) 
-   # Bk = 0.7832726 0.4284998 0.0007704066
-   # times in sec after modifications:
-   # 0.51
-   # 0.68 
+   FM_index <- pci(A1_clusters,A2_clusters)[2]
+   attr(FM_index, "E_FM") <- NA
+   attr(FM_index, "V_FM") <- NA
+   
+   return(FM_index)
 }
 
 
-# x = vector("list", 5)
-# x[1:2]=1:2
-# attr(x[[1]], "fo") = "mi"
-# attr(x[2], "fo") = "mi2"
-# str(x)
 
 
 
-Bk <- function(A1, A2, k = 3, print.output = F, sanity_checks = FALSE, ...)
+FM_index_R <- function(A1_clusters, A2_clusters, k,
+                     print_output = F, 
+               include_E = TRUE,
+               include_V = TRUE,               
+               ...)
+{
+
+   
+   # creating matrix M
+   # ----------
+   # a not-so-smart way
+   # M <- matrix(0, nrow = k, ncol = k)
+   # for(i in 1:k)
+   # {
+   # for(j in 1:k)
+   # {
+   # M[i,j] <- sum(rect.hclust(A1, k = k)[[i]] %in% rect.hclust(A2, k = k)[[j]])
+   # how many of the objects in cluser i in tree A1, exist in cluster j in tree A2
+   # }
+   # }
+   # ----------
+   # a much better way!
+   
+#    A1_clusters <- cutree(A1, k = k)
+#    A2_clusters <- cutree(A2, k = k)
+#    # calculate n
+#    n <- nleaves(A1) 
+   
+   # If one of the trees can not be cut, we should return the data.frame with missing values (so to not stop the entire function...)
+   if(any(is.na(A1_clusters)) || any(is.na(A2_clusters))) return(data.frame(Bk = NA, E_FM= NA, V_FM= NA) )
+   
+   # sort them to look the same (we assume the *same naming* for the leaves !
+   A1_clusters <- A1_clusters[order(names(A1_clusters))]	# order the vec accourding to the names, so to allow a comparison
+   A2_clusters <- A2_clusters[order(names(A2_clusters))]	# order the vec accourding to the names, so to allow a comparison
+   if(print_output) print(list(A1_clusters, A2_clusters )	)
+   M <- table(A1_clusters, A2_clusters)
+   if(print_output) print(M)
+   
+   
+#    if(!include_V) Bk_table$V_FM <- NULL
+   
+   # calculate n
+   n <- length(A1_clusters) 
+   
+   Tk <- sum(M^2) - n
+   m_i. <- apply(M, 1, sum)
+   m_.j <- apply(M, 2, sum)
+   m_.. <- n # sum(M)
+   if(sum(M) != n ) stop("Why does M matrix doesn't sum up to n ??")
+   Pk <- sum(m_i.^2) - n
+   Qk <- sum(m_.j^2) - n
+   
+   Bk <- Tk / sqrt(Pk*Qk)
+   
+   # Expectancy of the Bk (according to H0)
+   E_FM <- sqrt(Pk*Qk)/ (n*(n-1))
+   
+   
+   Pk2 <- sum( m_i. * (m_i. -1) * (m_i. -2) )
+   Qk2 <- sum( m_.j * (m_.j -1) * (m_.j -2) )
+   # variance of the Bk (according to H0)
+   V_FM <- 2/ (n*(n-1))  +  
+      4*Pk2 * Qk2 / ( (n*(n-1)*(n-2)) * Pk*Qk ) + 
+      (Pk -2 - 4*Pk2/Pk) * (Qk -2 - 4*Qk2/Qk) / ( (n*(n-1)*(n-2)*(n-3))) -
+      Pk * Qk / (n^2*(n-1)^2)
+   
+   
+   FM_index <- Bk # c(, E_FM, V_FM)   
+   attr(FM_index, "E_FM") <- ifelse(include_E, E_FM, NA)
+   attr(FM_index, "V_FM") <- ifelse(include_V, V_FM, NA)
+   
+   
+   
+   return(FM_index)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Bk <- function(A1, A2, k = 3, print_output = F, 
+               sanity_checks = FALSE, 
+               include_E = TRUE,
+               include_V = TRUE,               
+               ...)
 {
    if(sanity_checks) {	# the sanity checks are turned off by default since the "labels" function for dendrogram is one which takes some time to run...
       # notice that we must have labels.hclust and labels.dendrogram defined!
@@ -89,7 +271,7 @@ Bk <- function(A1, A2, k = 3, print.output = F, sanity_checks = FALSE, ...)
    A2_clusters <- cutree(A2, k = k)
    
    # If one of the trees can not be cut, we should return the data.frame with missing values (so to not stop the entire function...)
-   if(is.null(A1_clusters) || is.null(A2_clusters)) return(data.frame(Bk = NA, E_Bk= NA, Var_Bk= NA) )
+   if(is.null(A1_clusters) || is.null(A2_clusters)) return(data.frame(Bk = NA, E_FM= NA, V_FM= NA) )
    
    # sort them to look the same (we assume the *same naming* for the leaves !
    A1_clusters <- A1_clusters[order(names(A1_clusters))]	# order the vec accourding to the names, so to allow a comparison
@@ -109,23 +291,28 @@ Bk <- function(A1, A2, k = 3, print.output = F, sanity_checks = FALSE, ...)
    
    Bk <- Tk / sqrt(Pk*Qk)
    
-   E_Bk <- sqrt(Pk*Qk)/ (n*(n-1))
+   E_FM <- sqrt(Pk*Qk)/ (n*(n-1))
    
    
    Pk2 <- sum( m_i. * (m_i. -1) * (m_i. -2) )
    Qk2 <- sum( m_.j * (m_.j -1) * (m_.j -2) )
-   Var_Bk <- 2/ (n*(n-1))  +  
+   V_FM <- 2/ (n*(n-1))  +  
       4*Pk2 * Qk2 / ( (n*(n-1)*(n-2)) * Pk*Qk ) + 
       (Pk -2 - 4*Pk2/Pk) * (Qk -2 - 4*Qk2/Qk) / ( (n*(n-1)*(n-2)*(n-3))) -
       Pk * Qk / (n^2*(n-1)^2)
    
    
-   Bk_table <- data.frame(Bk, E_Bk, Var_Bk)
-   if(!include_E) Bk_table$E_Bk <- NULL
-   if(!include_V) Bk_table$Var_Bk <- NULL
+   Bk_table <- data.frame(Bk, E_FM, V_FM)
+   if(!include_E) Bk_table$E_FM <- NULL
+   if(!include_V) Bk_table$V_FM <- NULL
          
    return(Bk_table)
 }
+
+
+
+
+
 
 # Bk(fit1, fit2, k = 2, T)
 # Bk(as.dendrogram(fit1), as.dendrogram(fit2), k = 2, T)
@@ -190,7 +377,7 @@ Bk_range <- function(A1, A2, ks = NULL, to_print = F)
 
 # Bk_range(fit1, fit2, c(2:6))
 
-plot_Bks_with_E_Bk <- function(Bk_data_frame, CI_sd_times = 2, plot_mean_Bk_under_H0 = TRUE, ...)	
+plot_Bks_with_E_FM <- function(Bk_data_frame, CI_sd_times = 2, plot_mean_Bk_under_H0 = TRUE, ...)	
 {
    # CI_sd_times == how many times to multiply the Sd(Bk) - to get the cI
    # the three dots ... are for the plot
@@ -200,9 +387,9 @@ plot_Bks_with_E_Bk <- function(Bk_data_frame, CI_sd_times = 2, plot_mean_Bk_unde
    with(aa, plot(Bk ~ ks, ylim = c(0,1), type = "b" ,...))
    if(plot_mean_Bk_under_H0 )
    {
-      with(aa, points(E_Bk ~ ks, type = "l" , lwd = 2))
-      with(aa, points((E_Bk +  CI_sd_times*sqrt(Var_Bk))  ~ ks, type = "l" , lwd = 1, lty = 2))
-      with(aa, points((E_Bk -  CI_sd_times*sqrt(Var_Bk))  ~ ks, type = "l" , lwd = 1, lty = 2))
+      with(aa, points(E_FM ~ ks, type = "l" , lwd = 2))
+      with(aa, points((E_FM +  CI_sd_times*sqrt(V_FM))  ~ ks, type = "l" , lwd = 1, lty = 2))
+      with(aa, points((E_FM -  CI_sd_times*sqrt(V_FM))  ~ ks, type = "l" , lwd = 1, lty = 2))
    }
 }
 
@@ -215,9 +402,9 @@ plot_Bks_with_E_Bk <- function(Bk_data_frame, CI_sd_times = 2, plot_mean_Bk_unde
 
 
 
-plot_Bks_with_E_Bk.and.bakers.gamma <- function(the_bk_object ,the_bakers_gamma, main = "")
+plot_Bks_with_E_FM.and.bakers.gamma <- function(thE_FM_object ,the_bakers_gamma, main = "")
 {
-   plot_Bks_with_E_Bk(the_bk_object)
+   plot_Bks_with_E_FM(thE_FM_object)
    
    title(main = main)
    legend("topright", legend = paste("Baker's gamma (P_value):  ",
@@ -227,34 +414,34 @@ plot_Bks_with_E_Bk.and.bakers.gamma <- function(the_bk_object ,the_bakers_gamma,
 }
 
 
-# plot_Bks_with_E_Bk(Bk_range(fit1, fit2))	
+# plot_Bks_with_E_FM(Bk_range(fit1, fit2))	
 
 
-plot_Bks_with_E_Bk.BH.adjusted <- function(Bk_data_frame, CI_sd_times = 2, ...)	
+plot_Bks_with_E_FM.BH.adjusted <- function(Bk_data_frame, CI_sd_times = 2, ...)	
 {
    # CI_sd_times == how many times to multiply the Sd(Bk) - to get the cI
    # the three dots ... are for the plot
    aa <- Bk_data_frame
    
-   Bk.Ps <- with(aa, 1-pnorm(((Bk - E_Bk)/sqrt(Var_Bk)) ))
-   with(aa, 1-pnorm(((Bk - E_Bk)/sqrt(Var_Bk)) ))
-   with(aa, 1-pnorm(Bk , E_Bk,sqrt(Var_Bk)))
+   Bk.Ps <- with(aa, 1-pnorm(((Bk - E_FM)/sqrt(V_FM)) ))
+   with(aa, 1-pnorm(((Bk - E_FM)/sqrt(V_FM)) ))
+   with(aa, 1-pnorm(Bk , E_FM,sqrt(V_FM)))
    aa$Bk
    adjusted_Bk.Ps <- p.adjust(Bk.Ps, method = "BH")
-   adjusted_Bk <- with(aa, (qnorm(1-adjusted_Bk.Ps) + E_Bk)*sqrt(Var_Bk))	
+   adjusted_Bk <- with(aa, (qnorm(1-adjusted_Bk.Ps) + E_FM)*sqrt(V_FM))	
    ss <- adjusted_Bk == Inf
    adjusted_Bk[ss] <- aa$Bk[ss] # fill in Inf values with original Bk 
    
    
    with(aa, plot(Bk ~ ks, ylim = c(0,1), type = "b" ,...))
    with(aa, points(adjusted_Bk ~ ks, col = "orange", pch = 19, type = "b"))
-   with(aa, points(E_Bk ~ ks, type = "l" , lwd = 2))
-   with(aa, points((E_Bk +  CI_sd_times*sqrt(Var_Bk))  ~ ks, type = "l" , lwd = 1, lty = 2))
-   with(aa, points((E_Bk -  CI_sd_times*sqrt(Var_Bk))  ~ ks, type = "l" , lwd = 1, lty = 2))
+   with(aa, points(E_FM ~ ks, type = "l" , lwd = 2))
+   with(aa, points((E_FM +  CI_sd_times*sqrt(V_FM))  ~ ks, type = "l" , lwd = 1, lty = 2))
+   with(aa, points((E_FM -  CI_sd_times*sqrt(V_FM))  ~ ks, type = "l" , lwd = 1, lty = 2))
 }
 
 
-plot_Bks_with_E_Bk.holm.adjusted.CI <- function(Bk_data_frame, CI_sd_times = 2, ...)	
+plot_Bks_with_E_FM.holm.adjusted.CI <- function(Bk_data_frame, CI_sd_times = 2, ...)	
 {
    # CI_sd_times == how many times to multiply the Sd(Bk) - to get the cI
    # the three dots ... are for the plot
@@ -267,16 +454,16 @@ plot_Bks_with_E_Bk.holm.adjusted.CI <- function(Bk_data_frame, CI_sd_times = 2, 
    CI_sd_times <- qnorm(1-ordered.adj.Ps)	# the new  CI_sd_times - adjusted	
    
    with(aa, plot(Bk ~ ks, ylim = c(0,1), type = "b" ,...))	
-   with(aa, points(E_Bk ~ ks, type = "l" , lwd = 2))
-   with(aa, points((E_Bk +  CI_sd_times*sqrt(Var_Bk))  ~ ks, type = "l" , lwd = 1, lty = 2, col = "green"))
-   with(aa, points((E_Bk -  CI_sd_times*sqrt(Var_Bk))  ~ ks, type = "l" , lwd = 1, lty = 2, col = "green"))
+   with(aa, points(E_FM ~ ks, type = "l" , lwd = 2))
+   with(aa, points((E_FM +  CI_sd_times*sqrt(V_FM))  ~ ks, type = "l" , lwd = 1, lty = 2, col = "green"))
+   with(aa, points((E_FM -  CI_sd_times*sqrt(V_FM))  ~ ks, type = "l" , lwd = 1, lty = 2, col = "green"))
    
-   with(aa, points((E_Bk +  qnorm(.975)*sqrt(Var_Bk))  ~ ks, type = "l" , lwd = 1, lty = 2, col = 1))
-   with(aa, points((E_Bk -  qnorm(.975)*sqrt(Var_Bk))  ~ ks, type = "l" , lwd = 1, lty = 2, col = 1))	
+   with(aa, points((E_FM +  qnorm(.975)*sqrt(V_FM))  ~ ks, type = "l" , lwd = 1, lty = 2, col = 1))
+   with(aa, points((E_FM -  qnorm(.975)*sqrt(V_FM))  ~ ks, type = "l" , lwd = 1, lty = 2, col = 1))	
    
    bonf.CI_sd_times <- qnorm(1-((.05/2) / length(aa$Bk)))	
-   with(aa, points((E_Bk +  bonf.CI_sd_times*sqrt(Var_Bk))  ~ ks, type = "l" , lwd = 1, lty = 2, col = "red"))
-   with(aa, points((E_Bk -  bonf.CI_sd_times*sqrt(Var_Bk))  ~ ks, type = "l" , lwd = 1, lty = 2, col = "red"))
+   with(aa, points((E_FM +  bonf.CI_sd_times*sqrt(V_FM))  ~ ks, type = "l" , lwd = 1, lty = 2, col = "red"))
+   with(aa, points((E_FM -  bonf.CI_sd_times*sqrt(V_FM))  ~ ks, type = "l" , lwd = 1, lty = 2, col = "red"))
    
    legend("topright", fill = c("black", "red","green"), legend = c("no correction - +-1.96", "Bonferroni correction CI", "Holm's adjusted CI"))
 }
@@ -287,13 +474,50 @@ plot_Bks_with_E_Bk.holm.adjusted.CI <- function(Bk_data_frame, CI_sd_times = 2, 
 
 # calculate
 # aa <- Bk_range(fit1, fit2)
-# plot_Bks_with_E_Bk(aa)
+# plot_Bks_with_E_FM(aa)
 
 # str(as.dendrogram(fit1))
 # aa <- Bk_range(as.dendrogram(fit1), as.dendrogram(fit2))
 # Bk(as.dendrogram(fit1), as.dendrogram(fit2), k = 2,T)
 
-# plot_Bks_with_E_Bk(aa)
+# plot_Bks_with_E_FM(aa)
 
 # unlist(as.dendrogram(fit1))
 # labels(as.dendrogram(fit1))
+
+
+
+
+###
+#
+# Bk-method.r
+
+# require(R2PPT)
+
+
+
+##example:
+# mydata <- USArrests
+##Ward Hierarchical Clustering
+# d <- dist(mydata, method = "euclidean") # distance matrix
+# fit1 <- hclust(d, method="ward") 
+# fit2 <- hclust(d, method="single") 
+##fit <- fit1
+# par(mfrow = c(1,2))
+# plot(fit1) # display dendogram
+# plot(fit2) # display dendogram
+
+if(F) {
+   system.time(Bk(dendo1, dendo2)) 
+   # Bk = 0.7832726 0.4284998 0.0007704066
+   # times in sec after modifications:
+   # 0.51
+   # 0.68 
+}
+
+
+# x = vector("list", 5)
+# x[1:2]=1:2
+# attr(x[[1]], "fo") = "mi"
+# attr(x[2], "fo") = "mi2"
+# str(x)
