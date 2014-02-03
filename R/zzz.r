d86832f8ddc02b70a8f171a4d744c4e9de15d381
Tal Galili <tal.galili@gmail.com>
2014-02-03 12:58:21 +0200

    dendextend 0.12.2 is intended to be shipped to CRAN.

--- R/zzz.r
+++ R/zzz.r
@@ -189,6 +189,32 @@ dendextendWelcomeMessage <- function(){
 # +    'updateR.r'
 # +    'zzz.r'
 
+
+# Steps:
+# http://r.789695.n4.nabble.com/vignettes-problems-with-PDF-compaction-td4664909.html
+# 1) install gs - http://www.ghostscript.com/download/gsdnld.html
+# 2) find where it is, and update R_GSCMD:
+# Sys.setenv(R_GSCMD="C:\\Program Files\\gs\\gs9.10\\bin\\gswin64c.exe")
+# Sys.getenv("R_GSCMD")
+# 3) Check that it works: 
+# system2(Sys.getenv("R_GSCMD"), args="--version") 
+# 4) use:
+#### tools::compactPDF("inst\\doc\\dendextend-tutorial.pdf", gs_quality="ebook") 
+# tools::compactPDF("inst\\doc\\dendextend-tutorial.pdf") 
+###   compacted ‘dendextend-tutorial.pdf’ from 961Kb to 737Kb
+
+# For checking:
+# 1) get qpdf
+#     http://sourceforge.net/projects/qpdf/files/
+# 2) put it somewhere
+# 3) set R_QPDF
+#  Sys.setenv(R_QPDF="C:\\Rtools\\qpdf-5.1.1\\bin\\qpdf.exe")
+#  Sys.which(Sys.getenv("R_QPDF", "qpdf"))
+   
+# Also, make sure to add:
+# options(repos=c("http://cran.rstudio.com", "http://www.stats.ox.ac.uk/pub/RWin" ))
+# to D:\R\R-devel\etc\Rprofile.site
+
 # when a function is renamed, its document in man must be removed - otherwise it may cause problems with the built check (it will try to run the code in the example, and will fail.)
 # When all is done, run:
 # require(devtools)
@@ -198,7 +224,8 @@ dendextendWelcomeMessage <- function(){
 # check(build_args="--no-build-vignettes --no-manual", args = "--no-examples --no-build-vignettes --no-manual",  cran = FALSE, cleanup = FALSE)
 # check(build_args="--no-build-vignettes ", args = "--no-build-vignettes",  cran = FALSE, cleanup = FALSE)
 # check(args="--as-cran")
+# check("D:/Dropbox/aaaa good R code/AA - My packages/dendextend", args="--as-cran")
 #                 Thanks to: http://stackoverflow.com/questions/10017702/r-cmd-check-options-for-more-rigorous-testing-2-15-0
 # file.copy("NEWS", "NEWS.md")
-# build_win()
+# build_win(version="R-devel")
 # release()
