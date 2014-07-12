208af7d6e8bc43b0ca1dc405f176bebddba0e46a
Tal Galili <tal.galili@gmail.com>
2014-07-13 02:44:32 +0300

    update.dendrogram - added .Rd docs :)

--- R/update.dendrogram.R
+++ R/update.dendrogram.R
@@ -18,37 +18,60 @@
 
 
 
-#' @title Update dendrogram features
+#' @title Update a dendrogram
 #' @export
 #' @aliases 
-#' update.dendrogram
 #' update.dendlist
+#'
+#' @usage 
+#' \method{update}{dendrogram}(object,
+#'    what = c("labels",
+#'             "labels_colors",
+#'             "labels_cex",
+#'             "leaves_pch",
+#'             "leaves_cex",
+#'             "leaves_col",
+#'             "hang_leaves",
+#'             "branches_k_color",
+#'             "branches_col",
+#'             "branches_lwd",
+#'             "branches_lty",
+#'             "clear_branches",
+#'             "clear_leaves"
+#'    ),
+#'    with, ...)
+#'    
+#' \method{update}{dendlist}(object, ..., which)
+#'
 #' @description
 #' a master function for updating various attributes and 
 #' features of dendrogram objects.
 #' 
-#' @usage
-#' 
-#' \method{update}{dendrogram}(object, ...) 
-#' 
-#' \method{update}{dendlist}(object, which, ...) 
-#' 
-#' @param object a tree (dendrogram, or \link{dendlist})
+#' @param object a tree (\link{dendrogram}, or \link{dendlist})
 #' @param what a character indicating what property of
-#' the tree should be updated.
+#' the tree should be updated. (see the usage and the example section
+#' for the different options)
 #' @param with a varying object (it depends on the "what"),
 #' with it we will update the tree.
-#' @param which an integer indicating, in the case "object" is
+#' @param ... passed to the specific function for more options.
+#' @param which an integer vector indicating, in the case "object" is
 #' a dendlist, which of the trees should have 
 #' their "what" updated "with" something. If missing - the update
 #' will be performed on all of objects in the dendlist.
-#' @param ... passed to the specific function for more options.
 #' 
 #' @details
-#' ddd
+#' This is a wrapper function for many of the main tasks we 
+#' might wish to perform on a dendrogram before plotting.
 #' 
 #' @seealso
-#' \link{cophenetic}, \link{cor_bakers_gamma}
+#' 
+#' \link{labels<-.dendrogram}, \link{labels_colors<-},
+#' \link{hang.dendrogram}, \link{color_branches},
+#' \link{assign_values_to_leaves_nodePar},
+#' \link{assign_values_to_branches_edgePar},
+#' \link{remove_branches_edgePar}, \link{remove_leaves_nodePar},
+#' \link{dendrogram}
+#' 
 #' @return 
 #' An updated dendrogram (or dendlist), with some change to
 #' the parameters of it
@@ -59,149 +82,121 @@
 #' 
 #' set.seed(23235)
 #' ss <- sample(1:150, 10 )
-#' hc1 <- iris[ss,-5] %>% dist %>% hclust("com")
-#' hc2 <- iris[ss,-5] %>% dist %>% hclust("single")
-#' dend1 <- as.dendrogram(hc1)
-#' dend2 <- as.dendrogram(hc2)
-#' #    cutree(dend1)   
 #' 
-#' cophenetic(hc1)
-#' cophenetic(hc2)
-#' # notice how the dist matrix for the dendrograms have different orders:
-#' cophenetic(dend1)
-#' cophenetic(dend2)
+#' # Getting the dend object
+#' dend <- iris[ss,-5] %>% dist %>% hclust %>% as.dendrogram
+#' dend %>% plot
 #' 
-#' cor(cophenetic(hc1), cophenetic(hc2)) # 0.874
-#' cor(cophenetic(dend1), cophenetic(dend2))  # 0.16
-#' # the difference is becasue the order of the distance table in the case of
-#' # stats:::cophenetic.dendrogram will change between dendrograms!
+#' dend %>% labels
+#' dend %>% update("labels", 1:10) %>% labels
+#' dend %>% update("labels", 1:10) %>% plot 
+#' dend %>% update("labels_color") %>% plot 
+#' dend %>% update("labels_col", c(1,2)) %>% plot # Works also with partial matching :)
+#' dend %>% update("labels_cex", c(1, 1.2)) %>% plot 
+#' dend %>% update("leaves_pch", NA) %>% plot 
+#' dend %>% update("leaves_pch", c(1:5)) %>% plot    
+#' dend %>% update("leaves_pch", c(19,19, NA)) %>% 
+#'    update("leaves_cex", c(1,2)) %>% plot 
+#' dend %>% update("leaves_pch", c(19,19, NA)) %>% 
+#'    update("leaves_cex", c(1,2)) %>%
+#'    update("leaves_col", c(1,1,2,2)) %>% 
+#'    plot 
+#' dend %>% update("hang") %>% plot 
 #' 
-#' # however, this is consistant (since I force-sort the rows/columns): 
-#' cor_cophenetic(hc1, hc2)
-#' cor_cophenetic(dend1, dend2)
+#' dend %>% update("branches_k_col") %>% plot 
+#' dend %>% update("branches_k_col", c(1,2)) %>% plot 
+#' dend %>% update("branches_k_col", c(1,2,3), k=3) %>% plot
+#' dend %>% update("branches_k_col", k=3) %>% plot 
 #' 
-#' cor_cophenetic(dendlist(dend1, dend2))
+#' dend %>% update("branches_col", c(1,2, 1, 2, NA)) %>% plot
+#' dend %>% update("branches_lwd", c(2,1,2)) %>% plot
+#' dend %>% update("branches_lty", c(1,2,1)) %>% plot
 #' 
-#' # we can also use different cor methods (almost the same result though): 
-#' cor_cophenetic(hc1, hc2, method = "spearman") # 0.8456014
-#' cor_cophenetic(dend1, dend2, method = "spearman") # 
+#' #    clears all of the things added to the leaves
+#' dend %>% 
+#'    update("labels_color", c(19,19, NA)) %>% 
+#'    update("leaves_pch", c(19,19, NA))  %>%  # plot  
+#'    update("clear_leaves") %>% # remove all of what was done until this point
+#'    plot
+#' # Different order
+#' dend %>% 
+#'    update("leaves_pch", c(19,19, NA)) %>% 
+#'    update("labels_color", c(19,19, NA)) %>% 
+#'    update("clear_leaves") %>% plot
 #' 
 #' 
-#' # cophenetic correlation is about 10 times (!) faster than bakers_gamma cor:
-#' require(microbenchmark)
-#' microbenchmark(
-#'    cor_bakers_gamma = cor_bakers_gamma(dend1, dend2, try_cutree_hclust=FALSE),
-#'    cor_cophenetic = cor_cophenetic(dend1, dend2)   ,
-#'    times=10
-#' )
+#' # doing this without chaining (%>%) will NOT be fun:
+#' dend %>% 
+#'    update("labels", 1:10) %>%
+#'    update("labels_color") %>%
+#'    update("branches_col", c(1,2, 1, 2, NA)) %>%
+#'    update("branches_lwd", c(2,1,2)) %>%
+#'    update("branches_lty", c(1,2,1)) %>%
+#'    update("hang") %>%
+#'    plot 
 #' 
-#' # but only because of the cutree for dendrogram. When allowing hclust cutree
-#' # it is only about twice as fast:
-#' microbenchmark(
-#'    cor_bakers_gamma = cor_bakers_gamma(dend1, dend2, try_cutree_hclust=TRUE),
-#'    cor_cophenetic = cor_cophenetic(dend1, dend2)   ,
-#'    times=10
-#' )
+#' # A few dendlist examples:
+#' dendlist(dend,dend) %>% update("hang") %>% plot
+#' dendlist(dend,dend) %>% update("branches_k_col", k=3) %>% plot
+#' dendlist(dend,dend) %>% update("labels_col", c(1,2)) %>% plot
+#' 
+#' dendlist(dend,dend) %>% 
+#'    update("hang") %>%
+#'    update("labels_col", c(1,2), which = 1) %>% 
+#'    update("branches_k_col", k=3, which = 2) %>%
+#'    update("labels_cex", 1.2) %>%
+#'    plot
 #' 
 #' }
+update.dendrogram <- 
+   function(object,
+            what = c("labels",
+                     "labels_colors",
+                     "labels_cex",
+                     "leaves_pch",
+                     "leaves_cex",
+                     "leaves_col",
+                     "hang_leaves",
+                     "branches_k_color",
+                     "branches_col",
+                     "branches_lwd",
+                     "branches_lty",
+                     "clear_branches",
+                     "clear_leaves"
+            ),
+            with, ...){
+      what <- match.arg(what)
+      object <- switch(what, 
+                       #                     labels = dendextend:::`labels<-.dendrogram`(object, value = with)
+                       labels = `labels<-.dendrogram`(object, value = with, ...),
+                       labels_colors = `labels_colors<-`(object, value = with, ...),
+                       #      labels_colors = assign_values_to_leaves_nodePar(object, with, "lab.col", ...),
+                       labels_cex = assign_values_to_leaves_nodePar(object, with, "lab.cex", ...),
+                       leaves_pch = assign_values_to_leaves_nodePar(object, with, "pch", ...),
+                       leaves_cex =assign_values_to_leaves_nodePar(object, with, "cex", ...),
+                       leaves_col =assign_values_to_leaves_nodePar(object, with, "col", ...),
+                       hang_leaves = hang.dendrogram(dend = object, hang = ifelse(missing(with), .1, with),...),
+                       branches_k_color = color_branches(tree = object, col = with,  ...),
+                       branches_col = assign_values_to_branches_edgePar(object = object, value = with, edgePar = "col", ...),
+                       branches_lwd = assign_values_to_branches_edgePar(object = object, value = with, edgePar = "lwd", ...),
+                       branches_lty = assign_values_to_branches_edgePar(object = object, value = with, edgePar = "lty", ...),
+                       clear_branches = remove_branches_edgePar(object, ...),
+                       clear_leaves = remove_leaves_nodePar(object, ...)
+      )
+      object
+   }
 
 
-update.dendrogram <- function(object, 
-                              what = c("labels",
-                                       "labels_colors",
-                                       "labels_cex",
-                                       "leaves_pch",
-                                       "leaves_cex",
-                                       "leaves_col",
-                                       "hang_leaves",
-                                       "branches_k_color",
-                                       "branches_col",
-                                       "branches_lwd",
-                                       "branches_lty",
-                                       "clear_branches",
-                                       "clear_leaves"
-                                       
-                              ),
-                              with, ...){
-   what <- match.arg(what)
-   object <- switch(what, 
-#                     labels = dendextend:::`labels<-.dendrogram`(object, value = with)
-      labels = `labels<-.dendrogram`(object, value = with, ...),
-      labels_colors = `labels_colors<-`(object, value = with, ...),
-#      labels_colors = assign_values_to_leaves_nodePar(object, with, "lab.col", ...),
-      labels_cex = assign_values_to_leaves_nodePar(object, with, "lab.cex", ...),
-      leaves_pch = assign_values_to_leaves_nodePar(object, with, "pch", ...),
-      leaves_cex =assign_values_to_leaves_nodePar(object, with, "cex", ...),
-      leaves_col =assign_values_to_leaves_nodePar(object, with, "col", ...),
-      hang_leaves = hang.dendrogram(dend = object, hang = ifelse(missing(with), .1, with),...),
-      branches_k_color = color_branches(tree = object, col = with,  ...),
-      branches_col = assign_values_to_branches_edgePar(object = object, value = with, edgePar = "col", ...),
-      branches_lwd = assign_values_to_branches_edgePar(object = object, value = with, edgePar = "lwd", ...),
-      branches_lty = assign_values_to_branches_edgePar(object = object, value = with, edgePar = "lty", ...),
-      clear_branches = remove_branches_edgePar(object, ...),
-      clear_leaves = remove_leaves_nodePar(object, ...)
-   )
+#' @export
+#' @S3method update dendlist
+update.dendlist <- function(object, ..., which) {
    
+   if(missing(which)) which <- 1:length(object)
+   
+   for(i in which) {
+      object[[i]] <- update(object[[i]],...)      
+   }
    object
-}
-
-
-
-if(FALSE) {
-
-   set.seed(23235)
-   ss <- sample(1:150, 10 )
-   dend <- iris[ss,-5] %>% dist %>% hclust %>% as.dendrogram
-   
-   dend %>% labels
-   dend %>% update("labels", 1:10) %>% labels
-   dend %>% update("labels", 1:10) %>% plot # Works :)
-   dend %>% update("labels_color") %>% plot # Works :)
-   dend %>% update("labels_col", c(1,2)) %>% plot # Works :)
-   dend %>% update("labels_cex", c(1, 1.2)) %>% plot # Works :)
-   dend %>% update("branches_k_col") %>% plot # Works :)
-   dend %>% update("branches_k_col", c(1,2)) %>% plot # Works :)
-   dend %>% update("branches_k_col", c(1,2,3), k=3) %>% plot # Works :)
-   dend %>% update("branches_k_col", k=3) %>% plot # Works :)
-   dend %>% update("hang") %>% plot # Works :)
-   
-   
-   
-   dend %>% update("leaves_pch", NA) %>% plot # Works :)
-   dend %>% update("leaves_pch", c(1:5)) %>% plot # Works :)   
-   dend %>% update("leaves_pch", c(19)) %>% update("leaves_cex", c(1,2)) %>% plot # Works :)
-   dend %>% update("leaves_pch", c(19,19, NA)) %>% 
-      update("leaves_cex", c(1,2)) %>%
-      update("leaves_col", c(1,1,2,2)) %>% 
-      plot # Works :)
-   
-   #    clears all of the things added to the leaves
-   dend %>% 
-      update("labels_color", c(19,19, NA)) %>% 
-      update("leaves_pch", c(19,19, NA))  %>%  # plot  
-      update("clear_leaves") %>% # remove all of what was done until this point
-      plot
-   
-   dend %>% 
-      update("leaves_pch", c(19,19, NA)) %>%    
-      update("labels_color", c(2,19, NA)) %>% 
-      plot
-   
-   dend %>% 
-      update("leaves_pch", c(19,19, NA)) %>% 
-      update("labels_color", c(19,19, NA)) %>% 
-      update("clear_leaves") %>% plot
-   
-   
-   dend %>% 
-      update("labels", 1:10) %>%
-      update("labels_color") %>%
-      update("branches_col", c(1,2, 1, 2, NA)) %>%
-      update("branches_lwd", c(2,1,2)) %>%
-      update("branches_lty", c(1,2,1)) %>%
-      update("hang") %>%
-      plot # Works :)
-   
 }
 
 
