efaf335dffc21d231434aae005f9a812d70ee597
Tal Galili <tal.galili@gmail.com>
2015-01-26 23:16:38 +0200

    Add ggdendro example for creating a radial plot :)

--- R/ape.R
+++ R/ape.R
@@ -70,6 +70,18 @@ as.phylo <- function (x, ...)
 #' 
 #' # Also possible to with ggplot2 :)
 #' 
+#' library(dendextend)
+#' library(ggplot2)
+#' library(ggdendro)
+#' dend <- iris[1:30,-5] %>% dist %>% hclust %>% as.dendrogram
+#' # Rectangular lines
+#' ddata <- dend %>% dendro_data(type = "rectangle")
+#' p <- ggplot(segment(ddata)) + 
+#'    geom_segment(aes(x = x, y = y, xend = xend, yend = yend)) + 
+#'    coord_flip() + 
+#'    scale_y_reverse(expand = c(0.2, 0)) + coord_polar(theta="x")
+#' p
+#' 
 #' 
 #' 
 #' }
