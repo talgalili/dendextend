4e30954617604aa0579afd1ca9b6a982ddfc5672
Tal Galili <tal.galili@gmail.com>
2015-01-29 12:34:56 +0200

    Fix theme_dendro: no visible global function definition for 'element_text'

--- R/ggdendro.R
+++ R/ggdendro.R
@@ -202,7 +202,10 @@ theme_dendro <- function()
 {
    library(ggplot2)
    element_blank <- ggplot2::element_blank
-   ggplot2::theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+   element_text <- ggplot2::element_text
+   theme <- ggplot2::theme
+
+   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
                   panel.background = element_blank(), axis.title.x = element_text(colour = NA), 
                   axis.title.y = element_blank(), axis.text.x = element_blank(), 
                   axis.text.y = element_blank(), axis.line = element_blank(), 
