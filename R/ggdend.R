6548b02ab6071d5f2aa4d7874006663f69dbbb90
Tal Galili <tal.galili@gmail.com>
2015-09-06 22:22:03 +0300

    as.ggdend.dendrogram - can now handle different label heights. (for example, the ones produces when using hang.dendrogram)

--- R/ggdend.R
+++ R/ggdend.R
@@ -315,11 +315,13 @@ as.ggdend.dendrogram <- function (dend, type = c("rectangle", "triangle"), edge.
 
    the_lab.col <- get_leaves_edgePar_attr_par("lab.col") # like doing edgePar_attr[[1]] ["col"]
    the_lab.cex <- get_leaves_edgePar_attr_par("lab.cex") # like doing edgePar_attr[[1]] ["col"]
+   the_lab.height <- get_leaves_attr(dend, "height")
    if(is.null(the_lab.col)) the_lab.col <- NA
    if(is.null(the_lab.cex)) the_lab.cex <- NA
 
    ggdata$labels$col <- the_lab.col
    ggdata$labels$cex <- the_lab.cex
+   ggdata$labels$y <- the_lab.height
          # The above saves us from errors such as:
          # Error in eval(expr, envir, enclos) : object 'cex' not found
          # In addition: Warning message:
