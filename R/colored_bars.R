ce47da4122bdbc4a1f712783d6564c6de1503fdd
Tal Galili <tal.galili@gmail.com>
2016-01-01 16:46:33 +0200

    colored_bars - Major corrections to the way the bars are located automatically (it is now positioned underneath the labels automatically!)

--- R/colored_bars.R
+++ R/colored_bars.R
@@ -48,6 +48,38 @@ rescale <- function (x, to = c(0, 1), from = range(x, na.rm = TRUE))
 
 
 
+
+
+
+#    if(T) 2 else 1
+#    if(F) 2 else 1
+
+
+
+# This function is require in order to know the height of the rotated labels in the dendrogram!
+# Thanks to Prof. Brian Ripley
+# # http://r.789695.n4.nabble.com/strwidth-and-strheight-for-rotated-text-td839105.html
+rotated_str_dim <- function(s) {
+   cha <- s
+   xusr <- par("usr") 
+   xh <- strwidth(cha, cex = par("cex")) 
+   yh <- strheight(cha, cex = par("cex")) * 5/3 
+   tmp <- xh 
+   xh <- yh/(xusr[4]-xusr[3])*par("pin")[2] 
+   xh <- xh/ par("pin")[1] * (xusr[2]-xusr[1]) 
+   yh <- tmp/(xusr[2]-xusr[1])* par("pin")[1] 
+   yh <- yh/ par("pin")[2] * (xusr[4]-xusr[3]) 
+   c(xh = xh, yh = yh)
+}
+V_rotated_str_dim <- Vectorize(rotated_str_dim)
+max_labels_height <- function(s) {
+   # s <- paste0(dend_labels, " ")
+   a <- V_rotated_str_dim(s)
+   max(a["yh",]) 
+}
+
+
+
 #' @title Add colored bars to a dendrogram
 #' @export
 #' @description
@@ -56,9 +88,12 @@ rescale <- function (x, to = c(0, 1), from = range(x, na.rm = TRUE))
 #' categorization.
 #' @param colors Coloring of objects on the dendrogram. Either a vector (one color per object) 
 #' or a matrix (can also be an array or a data frame)
-#' with each column giving one color per object. 
-#' Each column will be plotted as a horizontal row of colors
+#' with each column giving one group with color per object. 
+#' Each column will be plotted as a horizontal row of colors (when horiz = FALSE)
 #' under the dendrogram.
+#' As long as the sort_by_labels_order paramter is TRUE (default), the colors vector/matrix should
+#' be provided in the order of the original data order (and it will be re-ordered automaticall to 
+#' the order of the dendrogram)
 #' @param dend a dendrogram object. If missing, the colors are plotted without and re-ordering
 #' (this assumes that the colors are already ordered based on the dend's labels)
 #' This is also important in order to get the correct height/location of the colored bars
@@ -78,11 +113,12 @@ rescale <- function (x, to = c(0, 1), from = range(x, na.rm = TRUE))
 #' in which case you would need to enter a number manually)
 #' If no dend is supplied - the default will be 0
 #' @param text_shift a dendrogram object 
-#' @param sort_by_labels_order logical(FALSE) - if TRUE, then the order of the 
+#' @param sort_by_labels_order logical(TRUE) - if TRUE (default), then the order of the 
 #' colored bars will be sorted based on the order needed to change the original
 #' order of the observations to the current order of the labels in the dendrogram.
-#' If FALSE (default) the colored bars are plotted as-is, based on the order
+#' If FALSE the colored bars are plotted as-is, based on the order
 #' of the colors vector.
+#' @param horiz logical (FALSE by default). Set to TRUE when using plot(dend, horiz = TRUE)
 #' @param ... ignored at this point.
 #' @author Steve Horvath \email{SHorvath@@mednet.ucla.edu},
 #' Peter Langfelder \email{Peter.Langfelder@@gmail.com},
@@ -112,7 +148,6 @@ rescale <- function (x, to = c(0, 1), from = range(x, na.rm = TRUE))
 #' \link[WGCNA]{plotDendroAndColors}
 #' @examples
 #' 
-#' \dontrun{
 #' 
 #' rows_picking <- c(1:5, 25:30)
 #' dend <- (iris[rows_picking,-5]*10) %>% dist %>% hclust %>% as.dendrogram 
@@ -129,10 +164,17 @@ rescale <- function (x, to = c(0, 1), from = range(x, na.rm = TRUE))
 #' k2 <- cutree(dend, k = 2)
 #' cols2 <- c("#0082CE", "#CC476B")[k2]
 #' plot(dend)
-#' # sadly, the shift paramteres need to be handled manually...
-#' colored_bars(cbind(cols2, cols), dend, y_shift = -1,
+#' colored_bars(cbind(cols2, cols), dend, 
+#'             rowLabels = c("2 clusters", "Odd numbers"))
+#' 
+#' # The same, but with an horizontal plot!
+#' par(mar = c(6,2,2,4))
+#' plot(dend, horiz = TRUE)
+#' colored_bars(cbind(cols2, cols), dend, 
 #'              rowLabels = c("2 clusters", "Odd numbers"),
-#'              text_shift = 1)
+#'               horiz = TRUE)
+#' 
+#' 
 #' 
 #' # let's add clusters color
 #' # notice how we need to play with the colors a bit
@@ -150,58 +192,62 @@ rescale <- function (x, to = c(0, 1), from = range(x, na.rm = TRUE))
 #' kx  <- sort_levels_values(kx[ord])   
 #' kx  <- kx[match(seq_along(ord), ord)]
 #' 
+#' par(mar = c(5,5,2,2))
+#' plot(dend)
 #' colored_bars(cbind(cols3[kx], cols2, cols), dend,
-#'              y_shift = -1, y_scale = 1.4,
-#'              rowLabels = c("3 clusters", "2 clusters", "Odd numbers"),
-#'              text_shift = 1)
-#' 
-#' 
-#' 
-#' 
-#' }
+#'              rowLabels = c("3 clusters", "2 clusters", "Odd numbers")) 
 #' 
 colored_bars <- function(colors, dend, rowLabels = NULL, cex.rowLabels = 0.9, 
                        add = TRUE, 
                        y_scale, y_shift,
                        text_shift = 1,
-                       sort_by_labels_order = FALSE,
+                       sort_by_labels_order = TRUE,
+                       horiz = FALSE,
                        #below_labels = TRUE,
                        ...) 
 {
+   # should either be a vector or a matrix
+   if(!(is.vector(colors) | is.matrix(colors))) stop("colors must be either a vector or a matrix")
    
-   ncol_colors <- ncol(colors)
-   num_of_rows <- if(is.null(ncol_colors)) 1 else ncol_colors
-   
-#    if(T) 2 else 1
-#    if(F) 2 else 1
-   
-   if(missing(dend) | !sort_by_labels_order) {
-      if(is.null(ncol_colors)) { # then colors is a vector
-         dend_order <- seq_along(colors)   
-      } else { # color is a matrix
-         dend_order <- seq_len(nrow(colors))
-      }
-      
-   } else {
+   # number of color boxes per row (i.e.: number of dend leaves)
+   n_colors <- if(is.vector(colors)) length(colors) else nrow(colors)
+   # number of rows (groups) of colors
+   n_groups <- if(is.vector(colors)) 1 else ncol(colors)
+
+   if(!missing(dend)) {
       # make sure we are working with a dend:
       if(is.hclust(dend)) dend <- as.dendrogram(dend) 
       if(!is.dendrogram(dend)) stop("'dend' should be a dendrogram.")   
       # get labels' order:
-      dend_order <- order.dendrogram(dend)      
+      # dend_size <- nleaves(dend)
+      dend_labels <- labels(dend)
+      dend_order <- order.dendrogram(dend)
+   } else { # if we ARE missing dend
+      dend_labels <- rep("W", n_colors) # assume all labels are W
+      dend_order <- seq_len(n_colors)
    }
+   
+   if(!sort_by_labels_order) dend_order <- seq_len(n_colors)
+   
+#    # Get y_shift to be underneath the labels
+#    if(!horiz) {
+#       if(missing(y_shift)) y_shift <- -max(strwidth(dend_labels))+par()$usr[3L]-2*strheight("x") # a bit of a hack, oh well...
+#       if(missing(y_scale)) y_scale <- median(strheight(dend_labels)) * nrow_colors * .8
+#    } else {
+#       if(missing(y_shift)) y_shift <- max(abs(strwidth(dend_labels)))+par()$usr[3L]-3*strwidth("x") # a bit of a hack, oh well...
+#       if(missing(y_scale)) y_scale <- median(abs(strwidth(dend_labels))) * nrow_colors * .8
+#    }
+
    
    # Get y_shift to be underneath the labels
-   if(missing(dend)) {
-      if(missing(y_shift)) y_shift <- 0
-      if(missing(y_scale)) y_scale <- 1 * num_of_rows
-      
+   if(!horiz) {
+      # We shift y with the max height of the rotated labels + plot region + one row of values (the height of X)
+      if(missing(y_shift)) y_shift <- -max_labels_height(dend_labels) + par("usr")[3L] - strheight("X")
+      if(missing(y_scale)) y_scale <- strheight("X") * n_groups # median(strheight(dend_labels)) * nrow_colors * .8
    } else {
-      labels_dend <- labels(dend)
-      if(missing(y_shift)) y_shift <- -max(strheight(labels_dend))+par()$usr[3L]-2*strheight("x") # a bit of a hack, oh well...
-      if(missing(y_scale)) y_scale <- median(strheight(labels_dend)) * num_of_rows
+      if(missing(y_shift)) y_shift <- max(abs(strwidth(dend_labels)))+par()$usr[3L]-3*strwidth("X") # a bit of a hack, oh well...
+      if(missing(y_scale)) y_scale <- median(abs(strwidth(dend_labels))) * nrow_colors * .8
    }
-   
-   
    
    
    
@@ -215,67 +261,99 @@ colored_bars <- function(colors, dend, rowLabels = NULL, cex.rowLabels = 0.9,
    y_shift <- y_shift-y_scale
 #    - if(below_labels) max(strheight(labels(dend))) else 0
    
-   colors <- as.matrix(colors);
+   colors <- as.matrix(colors)
    dimC <- dim(colors)
    
    if (is.null(rowLabels) & (length(dimnames(colors)[[2]])==dimC[2])) rowLabels = names(as.data.frame(colors));
    
    op <- options()
-   pr <- par()
+   pr <- par(no.readonly = TRUE)
    
    options(stringsAsFactors=FALSE);   
+   # par(mar = c(0, 4.1, 0, 2.1))
+   par(xpd = TRUE)
    
    if (length(dend_order) != dimC[1] ) 
       stop("ERROR: length of colors vector not compatible with number of objects in the hierarchical tree.");
-   nSets <- dimC[2];
-   C <- colors[dend_order, ]; 
-   step <- 1/(dimC[1]-1);
-   ystep <- 1/nSets;
+   C <- colors[dend_order, ] # new colors vector
+   step <- 1/(n_colors-1)
+   ystep <- 1/n_groups
    
-   old_mar <- par()$mar
-   par(mar = c(0, 4.1, 0, 2.1))
    if(!add) {
       barplot(height=1, col = "white", border=FALSE, space=0, axes=FALSE, ...)
    }
    
 
-   charWidth <- strwidth("W")/2;
-   for (j in 1:nSets)
+   charWidth <- strwidth("W")/2
+   charHeight <- strheight("W")/2
+   for (j in 1:n_groups)
    {
-      ind <- (1:dimC[1]);
-      xl <- (ind-1.5) * step; xr = (ind-0.5) * step; 
-      yb <- rep(ystep*(j-1), dimC[1]); yt = rep(ystep * j, dimC[1]);
+      ind <- (1:n_colors)
+      xl <- (ind-1.5) * step# locations of the x left
+      xr <- (ind-0.5) * step; 
+      yb <- rep(ystep*(j-1), n_colors)
+      yt <- rep(ystep * j, n_colors)
       
       if(add) {
-         n_leaves <- length(dend_order)
-         xl <- rescale(xl, to = c(1-.5, n_leaves-.5))
-         xr <- rescale(xl, to = c(1+.5, n_leaves+.5))
-         yb <- yb*y_scale + y_shift
+         xl <- rescale(xl, to = c(1-.5, n_colors-.5))
+         xr <- rescale(xl, to = c(1+.5, n_colors+.5))
+         yb <- yb + y_shift
          yt <- yt*y_scale + y_shift         
       }
       
-      if (is.null(dim(C))) {
-         rect(xl, yb, xr, yt, col = as.character(C), border = as.character(C));
-      } else {
-         rect(xl, yb, xr, yt, col = as.character(C[,j]), border = as.character(C[,j]));
+      if(horiz) {
+         
+         par(srt = 90)
+         
+         if (is.null(dim(C))) {
+            rect(-yb, xl, -yt, xr, col = as.character(C), border = as.character(C));
+         } else {
+            rect(-yb, xl, -yt, xr, col = as.character(C[,j]), border = as.character(C[,j]));
+         }
+         if (is.null(rowLabels))
+         {
+            text(as.character(j), adj = c(1,NA), y = charHeight*text_shift, x = -(ystep*(j-0.5)*y_scale + y_shift), cex=cex.rowLabels)
+            #          text(as.character(j), pos=2, x= -charWidth -0.5*step, y=ystep*(j-0.5), cex=cex.rowLabels, xpd = TRUE);
+         } else {
+            text(rowLabels[j], adj = c(1,NA), y = charHeight*text_shift, x = -(ystep*(j-0.5)*y_scale + y_shift), cex=cex.rowLabels)
+            #          text(rowLabels[j], pos=2, x= -charWidth -0.5*step, y=ystep*(j-0.5), cex=cex.rowLabels, xpd = TRUE);
+         } 
+         
+      } else { # default
+         if (is.null(dim(C))) {
+            rect(xl, yb, xr, yt, col = as.character(C), border = as.character(C));
+         } else {
+            rect(xl, yb, xr, yt, col = as.character(C[,j]), border = as.character(C[,j]));
+         }
+         if (is.null(rowLabels))
+         {
+            text(as.character(j), pos=2, x = charWidth*text_shift, y = ystep*(j-0.5)*y_scale + y_shift, cex=cex.rowLabels);
+            #          text(as.character(j), pos=2, x= -charWidth -0.5*step, y=ystep*(j-0.5), cex=cex.rowLabels, xpd = TRUE);
+         } else {
+            text(rowLabels[j], pos=2, x = charWidth*text_shift, y = ystep*(j-0.5)*y_scale + y_shift, cex=cex.rowLabels);
+            #          text(rowLabels[j], pos=2, x= -charWidth -0.5*step, y=ystep*(j-0.5), cex=cex.rowLabels, xpd = TRUE);
+         } 
       }
-      if (is.null(rowLabels))
-      {
-         text(as.character(j), pos=2, x=  charWidth*text_shift, y=ystep*(j-0.5)*y_scale + y_shift, cex=cex.rowLabels, xpd = TRUE);
-#          text(as.character(j), pos=2, x= -charWidth -0.5*step, y=ystep*(j-0.5), cex=cex.rowLabels, xpd = TRUE);
-      } else {
-         text(rowLabels[j], pos=2, x=  charWidth*text_shift, y=ystep*(j-0.5)*y_scale + y_shift, cex=cex.rowLabels, xpd = TRUE);
-#          text(rowLabels[j], pos=2, x= -charWidth -0.5*step, y=ystep*(j-0.5), cex=cex.rowLabels, xpd = TRUE);
-      } 
+      
    }
    # we start j from 0 so that it will add a line below the bars (and not just above them)
-   for (j in 0:nSets) {
-      the_x <- rescale(c(0,1), to = c(1-.5, n_leaves+.5))
-      lines(x=the_x, y=c(ystep*j,ystep*j)*y_scale + y_shift);
+
+   for (j in 0:n_groups) {
+      the_x <- rescale(c(0,1), to = c(1-.5, n_colors+.5))
+      
+      
+      if(horiz) { 
+         lines(y=the_x, x=-(c(ystep*j,ystep*j)*y_scale + y_shift));
+      } else {
+         lines(x=the_x, y=c(ystep*j,ystep*j)*y_scale + y_shift);
+      }
+      
    }
    
+   
+   
    options(op)     # reset (all) initial options   
-   par(mar = old_mar)
+   par(pr) # suppressWarnings(par(pr))
    
    return(invisible(C))
 }
