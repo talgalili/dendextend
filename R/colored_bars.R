a89df04610b701ced4d0f69cb04ede8ce1e3535b
Tal Galili <tal.galili@gmail.com>
2016-01-01 18:27:06 +0200

    colored_bars - updated example (a new mtcars example)

--- R/colored_bars.R
+++ R/colored_bars.R
@@ -197,6 +197,45 @@ max_labels_height <- function(s) {
 #' colored_bars(cbind(cols3[kx], cols2, cols), dend,
 #'              rowLabels = c("3 clusters", "2 clusters", "Odd numbers")) 
 #' 
+#' 
+#' 
+#' ## mtcars example
+#' 
+#' # Create the dend:
+#' dend <- as.dendrogram(hclust(dist(mtcars)))
+#' 
+#' # Create a vector giving a color for each car to which company it belongs to
+#' car_type <- rep("Other", length(rownames(mtcars)))
+#' is_x <- grepl("Merc", rownames(mtcars))
+#' car_type[is_x] <- "Mercedes"
+#' is_x <- grepl("Mazda", rownames(mtcars))
+#' car_type[is_x] <- "Mazda"
+#' is_x <- grepl("Toyota", rownames(mtcars))
+#' car_type[is_x] <- "Toyota"
+#' car_type <- factor(car_type)
+#' n_car_types <- length(unique(car_type))
+#' col_car_type <- colorspace::rainbow_hcl(n_car_types, c = 70, l  = 50)[car_type]
+#' 
+#' # extra: showing the various clusters cuts 
+#' k234 <- cutree(dend, k = 2:4)
+#' 
+#' # color labels by car company:
+#' labels_colors(dend) <- col_car_type[order.dendrogram(dend)]
+#' # color branches based on cutting the tree into 4 clusters:
+#' dend <- color_branches(dend, k = 4)
+#' 
+#' ### plots
+#' par(mar = c(12,4,1,1))
+#' plot(dend)
+#' colored_bars(cbind(k234[,3:1], col_car_type), dend, rowLabels = c(paste0("k = ", 4:2), "Car Type"))
+#' 
+#' # horiz version:
+#' par(mar = c(4,1,1,12))
+#' plot(dend, horiz = TRUE)
+#' colored_bars(cbind(k234[,3:1], col_car_type), dend, rowLabels = c(paste0("k = ", 4:2), "Car Type"), horiz = TRUE)
+#' 
+#' 
+#' 
 colored_bars <- function(colors, dend, rowLabels = NULL, cex.rowLabels = 0.9, 
                        add = TRUE, 
                        y_scale, y_shift,
