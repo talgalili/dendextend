7af67a36e599977549295bd9651c1043127937a0
Tal Galili <tal.galili@gmail.com>
2016-04-25 13:11:29 +0300

    Use test_check instead of test_package

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,11 +1,20 @@
 library(testthat)
 
 library(dendextend)
-test_package("dendextend")
+test_check("dendextend")
 
 library(dendextendRcpp)
-test_package("dendextend")
+test_check("dendextend")
 
+
+
+# 
+# library(dendextend)
+# test_package("dendextend")
+# 
+# library(dendextendRcpp)
+# test_package("dendextend")
+# 
 # 
 # library(dendextend)
 # tryCatch(test_package("dendextend"), 
