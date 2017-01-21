28ec9985b149d89561efd0eacd3f490a65b91657
Tal Galili <tal.galili@gmail.com>
2017-01-21 12:34:35 +0200

    New functions has_edgePar, has_nodePar - Does a dendrogram has an edgePar/nodePar component?

---
+++ R/has_edgePar.R
@@ -0,0 +1,50 @@
+
+
+#' Does a dendrogram has an edgePar/nodePar component?
+#' @export
+#' @aliases 
+#' has_edgePar
+#' has_nodePar
+#' 
+#' @param dend a \link{dendrogram} object.
+#' @param component a character value to be checked if exists in the tree. For edgePar the list: "col", "lty" and "lwd" (for the segments), 
+#' "p.col", "p.lwd", and "p.lty" (for the polygon around the text) and "t.col" for the text color.
+#' For edgePar "pch", "cex", "col", "xpd", and/or "bg".
+#' @param ... ignored
+#'
+#' @seealso \link{get_nodes_attr}, \link{set}
+#' @return
+#' Logical. TRUE if such a component is defined somewhere in the tree, FALSE otherwise.
+#'
+#' @examples
+#' 
+#' dat <- iris[1:20,-5]
+#' hca <- hclust(dist(dat))
+#' hca2 <- hclust(dist(dat), method = "single")
+#' dend <- as.dendrogram(hca)
+#' dend2 <- as.dendrogram(hca2)
+#' 
+#' dend %>% set("branches_lwd", 2) %>% set("branches_lty", 2) %>% plot
+#' dend %>% set("branches_lwd", 2) %>% set("branches_lty", 2) %>% has_edgePar("lty")
+#' dend %>% set("branches_lwd", 2) %>% has_edgePar("lty")
+#' dend %>% set("branches_lwd", 2) %>% has_edgePar("lwd")
+#' 
+has_component_in_attribute <- function(dend, component, the_attrib = "edgePar", ...) {
+   list_of_names <- dend %>% get_nodes_attr(the_attrib, simplify = FALSE) %>% lapply(names)
+   
+   has_component_fun <- function(node_names) any(node_names %in% component)
+   has_component <- list_of_names %>% lapply(has_component_fun) %>% unlist %>% any
+   has_component
+}
+
+#' @export
+has_edgePar <- function(dend, component, the_attrib = "edgePar", ...) {
+   has_component_in_attribute(dend, component, the_attrib, ...)
+}
+
+#' @export
+has_nodePar <- function(dend, component, the_attrib = "nodePar", ...) {
+   has_component_in_attribute(dend, component, the_attrib, ...)
+}
+
+
