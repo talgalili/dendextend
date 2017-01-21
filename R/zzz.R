89a52f4c29765041d1c8b3a7d15bf986800ba8da
Tal Galili <tal.galili@gmail.com>
2017-01-21 13:07:52 +0200

    Namespace in Imports field not imported from: 'datasets'

--- R/zzz.R
+++ R/zzz.R
@@ -20,6 +20,7 @@
 # http://stackoverflow.com/questions/10325231/when-writing-my-own-r-package-i-cant-seem-to-get-other-packages-to-import-corr
 #' @importFrom viridis viridis
 
+#' @import datasets
 
 
 
