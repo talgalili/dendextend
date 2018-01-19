e02364d83770f07e18e4bceeaccd53991b03c357
Tal Galili <tal.galili@gmail.com>
2018-01-19 21:46:03 +0200

    styler the functions

--- R/get_subdendrograms.R
+++ R/get_subdendrograms.R
@@ -3,18 +3,18 @@
 #' @export
 #' @description
 #' Extracts a list of subdendrogram structures based on the cutree \code{\link{cutree.dendrogram}} function
-#' from a given dendrogram object. It can be useful in case of more exact visual 
+#' from a given dendrogram object. It can be useful in case of more exact visual
 #' investigation of clustering results.
-#' @param dend a dendrogram object 
+#' @param dend a dendrogram object
 #' @param k the number of subdendrograms that should be extracted
 #' @param ... parameters that should be passed to the cutree
 #' \code{\link{cutree.dendrogram}}
-#' @return 
+#' @return
 #' A list of \emph{k} subdendrograms, based on the cutree
 #' \code{\link{cutree.dendrogram}} clustering
 #' clusters.
 #' @examples
-#' 
+#'
 #' # define dendrogram object to play with:
 #' dend <- iris[,-5] %>% dist %>% hclust %>% as.dendrogram %>%  set("labels_to_character") %>% color_branches(k=5)
 #' dend_list <- get_subdendrograms(dend, 5)
@@ -23,14 +23,14 @@
 #' par(mfrow = c(2,3))
 #' plot(dend, main = "Original dendrogram")
 #' sapply(dend_list, plot)
-#' 
+#'
 get_subdendrograms <- function(dend, k, ...) {
-    clusters <- cutree(dend, k, ...)
-    dend_list <- lapply(unique(clusters), function(cluster.id){
-       find_dendrogram(dend, which(clusters == cluster.id))
-    })  
-    class(dend_list) <- "dendlist"
-    dend_list
+  clusters <- cutree(dend, k, ...)
+  dend_list <- lapply(unique(clusters), function(cluster.id) {
+    find_dendrogram(dend, which(clusters == cluster.id))
+  })
+  class(dend_list) <- "dendlist"
+  dend_list
 }
 
 #' @title Search for the subdendrogram structure composed of indicated labels
@@ -39,16 +39,16 @@ get_subdendrograms <- function(dend, k, ...) {
 #' Given a dendrogram object, the function performs a recursive DFS algorithm to determine
 #' the subdendrogram which is composed of all indicated labels. The labels
 #' which should compose the subdendrogram are marked as TRUE in the logical
-#' vector of length \code{nleaves(dend)} 
-#' @param dend a dendrogram object 
+#' vector of length \code{nleaves(dend)}
+#' @param dend a dendrogram object
 #' @param selected_labels logical vector with TRUE values at positions of
 #' members which should be included in the resulting subdendrogram
-#' @return 
+#' @return
 #' A subdendrogram composed of only members indicated in the given logical
 #' vector
 #' clusters.
 #' @examples
-#' 
+#'
 #' \dontrun{
 #' # define dendrogram object to play with:
 #' dend <- iris[,-5] %>% dist %>% hclust %>% as.dendrogram %>%  set("labels_to_character") %>% color_branches(k=5)
@@ -59,15 +59,15 @@ get_subdendrograms <- function(dend, k, ...) {
 #' plot(dend, main = "Original dendrogram")
 #' plot(sub.dend, main = "First subdendrogram")
 #' }
-#' 
-find_dendrogram<-function(dend, selected_labels){
-    if(all(unlist(dend) %in% selected_labels))
-        return(dend)
-    
-    if(any(unlist(dend[[1]]) %in% selected_labels))
-        return(find_dendrogram(dend[[1]], selected_labels))
-    else 
-        return(find_dendrogram(dend[[2]], selected_labels))
+#'
+find_dendrogram <- function(dend, selected_labels) {
+  if (all(unlist(dend) %in% selected_labels)) {
+    return(dend)
+  }
+
+  if (any(unlist(dend[[1]]) %in% selected_labels)) {
+    return(find_dendrogram(dend[[1]], selected_labels))
+  } else {
+    return(find_dendrogram(dend[[2]], selected_labels))
+  }
 }
-
-
