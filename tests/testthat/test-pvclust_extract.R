805af674d56143e37040db1ca5134d43487496e0
C2H4 <bann13@gmail.com>
2019-03-14 10:34:34 -0400

    updated test-pvclust_extract.R (refer to Issues #83)

--- tests/testthat/test-pvclust_extract.R
+++ tests/testthat/test-pvclust_extract.R
@@ -8,6 +8,6 @@ dend <- USArrests[1:5,] %>% pvclust
 #pvclust_edges(dend)
 expect_identical(as.character(pvclust_edges(dend)[,1]), c("Assault","UrbanPop","Murder"))
 expect_identical(as.character(pvclust_edges(dend)[,2]), c("Rape","1","2"))
-#expect_identical(round(pvclust_edges(dend)[,3], digits = 2), c(0.97,1.00,1.00)) #there is a stochastic element to the outcome so while it works using test_that it may be causing error with travis-ci checks
+expect_identical(round(pvclust_edges(dend)[,3], digits = 2), c(0.97,1.00,1.00)) #there is a stochastic element to the outcome so while it works using test_that it may be causing error with travis-ci checks
 
 })
