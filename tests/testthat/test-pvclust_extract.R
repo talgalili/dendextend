ef372af2ebf5b513d09c111e743dba1e6ce318a0
C2H4 <bann13@gmail.com>
2019-03-13 23:03:40 -0400

    updated test case (refer to Issues #83)

--- tests/testthat/test-pvclust_extract.R
+++ tests/testthat/test-pvclust_extract.R
@@ -4,9 +4,9 @@ test_that("extract au",{
   set.seed(123)
 dend <- USArrests[1:5,] %>% pvclust
 #plot(dend)
-#pvclust.edges(dend)
-expect_identical(as.character(pvclust.edges(dend)[,1]), c("Assault","UrbanPop","Murder"))
-expect_identical(as.character(pvclust.edges(dend)[,2]), c("Rape","1","2"))
-#expect_identical(round(pvclust.edges(dend)[,3], digits = 2), c(0.97,1.00,1.00)) #there is a stochastic element to the outcome so while it works using test_that it may be causing error with travis-ci checks
+#pvclust_edges(dend)
+expect_identical(as.character(pvclust_edges(dend)[,1]), c("Assault","UrbanPop","Murder"))
+expect_identical(as.character(pvclust_edges(dend)[,2]), c("Rape","1","2"))
+#expect_identical(round(pvclust_edges(dend)[,3], digits = 2), c(0.97,1.00,1.00)) #there is a stochastic element to the outcome so while it works using test_that it may be causing error with travis-ci checks
 
 })
