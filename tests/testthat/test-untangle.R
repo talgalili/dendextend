596a3b44b9df02a442b5bdbc28a4ac3be759734d
alecbuetow <alecbuetow@gmail.com>
2024-10-29 18:50:11 -0400

    corrected error in untangle_evolution test

--- tests/testthat/test-untangle.R
+++ tests/testthat/test-untangle.R
@@ -499,13 +499,13 @@ test_that("untangle_evolution works", {
    sister_2_dend2 <- as.dendrogram(hclust(dist(iris[20:11, -5]), method = "ward.D2"))
    
    # determine which set of dendrograms can be better untangled
-   intercourse_result <- untangle_intercourse(brother_1_dend1, brother_1_dend2, brother_2_dend1, brother_2_dend2, L = 1)
+   intercourse_result <- untangle_intercourse(brother_1_dend1, brother_1_dend2, sister_2_dend1, sister_2_dend2, L = 1)
    entanglement_child1 <- entanglement(intercourse_result[[1]][[1]], intercourse_result[[1]][[2]])
    entanglement_child2 <- entanglement(intercourse_result[[2]][[1]], intercourse_result[[2]][[2]])
    expect_true(entanglement_child1 > entanglement_child2)
    # the untangled version of the better dendrograms should be the result
-   evolution_result <- untangle_evolution(brother_1_dend1, brother_1_dend2, brother_2_dend1, brother_2_dend2, L = 1)
-   expect_identical(evolution_result, intercourse_result[[2]])
+   evolution_result <- untangle_evolution(brother_1_dend1, brother_1_dend2, brother_1_dend1, brother_1_dend2, L = 1)
+   expect_identical(evolution_result, intercourse_result[[1]])
 })
 
 
