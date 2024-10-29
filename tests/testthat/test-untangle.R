251c4934b29eda5ce034a15b84827fceff2c747a
alecbuetow <alecbuetow@gmail.com>
2024-10-29 09:24:56 -0400

    corrected test for untangle_step_rotate_1side

--- tests/testthat/test-untangle.R
+++ tests/testthat/test-untangle.R
@@ -66,12 +66,12 @@ test_that("untangle_step_rotate_1side work", {
   expect_identical(round(entanglement(dend1, dend2, L = 2), 2), 0.47)
 
   # Fixing the problem :)
-  dend2_corrected <- untangle_step_rotate_1side(dend2, dend1)[[1]]
+  dend2_corrected <- untangle.dendrogram(dend2, dend1, "step1side", leaves_matching_method = "order")[[1]]
   #    tanglegram(dend1,dend2_corrected) # FIXED.
   expect_identical(round(entanglement(dend1, dend2_corrected, L = 2), 2), 0)
 
   # the other direction may also work:
-  dend2_corrected <- untangle_step_rotate_1side(dend2, dend1, direction = "backward")[[1]]
+  dend2_corrected <- untangle.dendrogram(dend2, dend1, "step1side", direction = "backward")[[1]]
   #    tanglegram(dend1,dend2_corrected) # FIXED.
   expect_identical(round(entanglement(dend1, dend2_corrected, L = 2), 2), 0)
 })
