4124e827bab80156e40c73e6226f35341fac195e
alecbuetow <alecbuetow@gmail.com>
2024-10-29 16:50:08 -0400

    added test for untangle_evolution

--- tests/testthat/test-untangle.R
+++ tests/testthat/test-untangle.R
@@ -464,3 +464,23 @@ test_that("entanglement_return_best_brother works", {
    expect_true(entanglement(brother_1_dend1, brother_1_dend2) > entanglement(brother_2_dend1, brother_2_dend2))
    expect_identical(result, dendlist(brother_2_dend1, brother_2_dend2))
 })
+
+
+
+
+test_that("untangle_intercourse_evolution works", {
+   suppressWarnings(RNGversion("3.5.0"))
+   set.seed(1)   
+   
+   brother_1_dend1 <- as.dendrogram(hclust(dist(iris[1:10, -5]), method = "complete"))
+   brother_1_dend2 <- as.dendrogram(hclust(dist(iris[10:1, -5]), method = "single"))
+   
+   brother_2_dend1 <- as.dendrogram(hclust(dist(iris[11:20, -5]), method = "average"))
+   brother_2_dend2 <- as.dendrogram(hclust(dist(iris[20:11, -5]), method = "ward.D2"))
+   
+   intercourse = list(dendlist(brother_1_dend1, brother_1_dend2), dendlist(brother_2_dend1, brother_2_dend2))
+   result <- untangle_intercourse_evolution(intercourse, L = 1)
+   # brother_1 is more entangled, therefore we expect to get brother_2 in result 
+   expect_true(entanglement(brother_1_dend1, brother_1_dend2) > entanglement(brother_2_dend1, brother_2_dend2))
+   expect_identical(result, dendlist(brother_2_dend1, brother_2_dend2))
+})
