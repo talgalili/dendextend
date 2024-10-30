81eddbdecfc09eee154f2ebe0d23710c229c987d
alecbuetow <alecbuetow@gmail.com>
2024-10-30 15:51:57 -0400

    determining whether tests for zzz.R are valid

---
+++ tests/testthat/test-zzz.R
@@ -0,0 +1,33 @@
+# cat("\n")
+# library(testthat)
+
+
+context("Loading dendextend package")
+
+
+
+test_that(".onLoad works", {
+   # no dendextend options yet 
+   .onLoad("dendextend", "dendextend")  
+   expect_null(getOption("dendextend"))  
+})
+
+
+
+test_that(".onUnload works", {
+   
+   # dendextend::tanglegram should no longer be available after Namespace is unloaded.
+   # HOWEVER, unloadNamespace is unimplemented, so dendextend::tanglegram persists
+   loadNamespace("dendextend")
+   expect_true(typeof(dendextend::tanglegram) == "closure")
+   unloadNamespace("dendextend")
+   expect_true(typeof(dendextend::tanglegram) == "closure")
+   
+   # tanglegram should no longer be available after detaching package
+   library(dendextend)
+   expect_true(typeof(tanglegram) == "closure")
+   detach("package:dendextend", unload = TRUE, character.only = TRUE)
+   expect_error(tanglegram)
+   
+})
+
