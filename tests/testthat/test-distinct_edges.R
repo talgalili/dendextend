41646fb88817d7bd314d1413bb8591bc7685fede
alecbuetow <alecbuetow@gmail.com>
2024-10-30 14:42:48 -0400

    added coverage for distinct_edges.R

--- tests/testthat/test-distinct_edges.R
+++ tests/testthat/test-distinct_edges.R
@@ -15,6 +15,10 @@ test_that("partition_leaves works", {
   #    partition_leaves(x)
   #    plot(x)
   expect_identical(partition_leaves(x), list(c(3L, 1L, 2L), 3L, 1:2, 1L, 2L))
+  
+  # check case where something other than a dendrogram is passed in
+  x <- matrix(1:4, nrow=2)
+  expect_error(partition_leaves(x))
 })
 
 
@@ -50,4 +54,63 @@ test_that("dist.dendlist work", {
       c("1", "2", "3"), c("1", "2", "3")
     ))
   )
+  
+  # check case where something other than a dendrogram is passed in
+  x <- matrix(1:4, nrow=2)
+  expect_error(dist.dendlist(x))
+})
+
+
+
+test_that("highlight_distinct_edges.dendrogram works", {
+   dend1 <- as.dendrogram(hclust(dist(iris[1:5, -5]), method = "average"))
+   dend2 <- as.dendrogram(hclust(dist(iris[2:6, -5]), method = "average"))
+   
+   # check that edge color was indeed changed to identify which edges differ
+   result <- highlight_distinct_edges.dendrogram(dend1, dend2, value = 3, edgePar = "col")
+   expect_identical(attributes(result)$edgePar$col, 3)
+})
+
+
+
+test_that("highlight_distinct_edges.dendlist works", {
+   dend1 <- as.dendrogram(hclust(dist(iris[1:5, -5]), method = "average"))
+   dend2 <- as.dendrogram(hclust(dist(iris[2:6, -5]), method = "average"))
+   dend_list <- dendlist(dend1, dend2)
+   
+   # check that edge color was indeed changed to identify which edges differ for both inserted dendrograms
+   result <- highlight_distinct_edges.dendlist(dend_list, value = 3, edgePar = "col")
+   expect_identical(attributes(result[[1]])$edgePar$col, 3)
+   expect_identical(attributes(result[[2]])$edgePar$col, 3)
+})
+
+
+
+test_that("dend_diff.dendrogram works", {
+   dend1 <- as.dendrogram(hclust(dist(iris[1:5, -5]), method = "average"))
+   dend2 <- as.dendrogram(hclust(dist(iris[2:6, -5]), method = "average"))
+   
+   # check that edge color was indeed changed to identify which edges differ
+   pdf(file = NULL)
+   capture.output(suppressWarnings(
+      result <- dend_diff.dendrogram(dend1, dend2, value = 3, edgePar = "col")
+   ))
+   dev.off()
+   expect_identical(result, dendlist(dend1, dend2))
+})
+
+
+
+test_that("dend_diff.dendlist works", {
+   dend1 <- as.dendrogram(hclust(dist(iris[1:5, -5]), method = "average"))
+   dend2 <- as.dendrogram(hclust(dist(iris[2:6, -5]), method = "average"))
+   dend_list <- dendlist(dend1, dend2)
+   
+   # check that edge color was indeed changed to identify which edges differ
+   pdf(file = NULL)
+   capture.output(suppressWarnings(
+      result <- dend_diff.dendlist(dend_list, value = 3, edgePar = "col")
+   ))
+   dev.off()
+   expect_identical(result, dendlist(dend1, dend2))
 })
