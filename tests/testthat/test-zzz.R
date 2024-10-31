28658b60fa72b8fd10559295fb8581201d2f6b9c
alecbuetow <alecbuetow@gmail.com>
2024-10-30 20:49:16 -0400

    attempting to cover onUnload and detach hooks

--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -9,5 +9,8 @@ test_that("onLoad works", {
    # no options for dendextend yet, should be empty
    dendextend:::.onLoad()
    expect_identical(getOption("dendextend"), NULL)
+   
+   dendextend:::assign_dendextend_options()
+   dendextend:::remove_dendextend_options()
 })
 
