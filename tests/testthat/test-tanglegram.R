48f738648cb5a9433412cd22484c1150bfee669b
alecbuetow <alecbuetow@gmail.com>
2024-11-04 13:21:11 -0500

    added test for tanglegram

--- tests/testthat/test-tanglegram.R
+++ tests/testthat/test-tanglegram.R
@@ -10,5 +10,30 @@ test_that("plot_horiz.dendrogram works", {
      dist() %>%
      hclust() %>%
      as.dendrogram()
-   plot_horiz.dendrogram(dend, side = F)
+   
+   expect_no_error(
+      plot_horiz.dendrogram(dend, side = F)
+   )
+   
+})
+
+
+test_that("tanglegram works", {
+   
+   set.seed(23235)
+   ss <- sample(1:150, 10)
+   dend1 <- iris[ss, -5] %>%
+     dist() %>%
+     hclust("com") %>%
+     as.dendrogram()
+   dend2 <- iris[ss, -5] %>%
+     dist() %>%
+     hclust("sin") %>%
+     as.dendrogram()
+   dend12 <- dendlist(dend1, dend2)
+   
+   expect_no_error(
+      tanglegram(dend12, common_subtrees_color_branches = TRUE)
+   )
+   
 })
