177d9d5a35fe15f13b25760ac3065e174d9dd0aa
alecbuetow <alecbuetow@gmail.com>
2024-11-04 13:30:30 -0500

    added tests for tanglegram

--- tests/testthat/test-tanglegram.R
+++ tests/testthat/test-tanglegram.R
@@ -19,8 +19,9 @@ test_that("plot_horiz.dendrogram works", {
 
 
 test_that("tanglegram works", {
-   
+   suppressWarnings(RNGversion("3.5.0"))
    set.seed(23235)
+   
    ss <- sample(1:150, 10)
    dend1 <- iris[ss, -5] %>%
      dist() %>%
@@ -35,5 +36,17 @@ test_that("tanglegram works", {
    expect_no_error(
       tanglegram(dend12, common_subtrees_color_branches = TRUE)
    )
+   
+   # if non-dendrogram objects are passed in
+   expect_no_error(
+      tanglegram(as.hclust(dend1), as.hclust(dend2))
+   )
+   expect_no_error(
+      tanglegram(ape::as.phylo(dend1), ape::as.phylo(dend2))
+   )
+   expect_error(
+      tanglegram(1:4, 4:1)
+   )
+   
    
 })
