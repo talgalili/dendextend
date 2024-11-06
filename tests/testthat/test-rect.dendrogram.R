6bdaf7c366d8ba2b7ddb50cf5361b37778c9633a
Alec Buetow <97068347+alecbuetow@users.noreply.github.com>
2024-11-06 16:49:33 -0500

    Added tests for rect.dendrogram (#140)
    * added test for rect.dendrogram
    
    * added tests for rect.dendrogram
    
    * added test for identify.dendrogram
    
    * finished coverage for rect.dendrogram

--- tests/testthat/test-rect.dendrogram.R
+++ tests/testthat/test-rect.dendrogram.R
@@ -1,26 +1,92 @@
 # library(testthat)
-#
-# context("rect.dendrogram")
-#
-# test_that("rect.dendrogram work",{
-#
-# #    dend15 <- c(1:5) %>% dist %>% hclust(method = "average") %>% as.dendrogram
-# #    # dend15 <- c(1:25) %>% dist %>% hclust(method = "average") %>% as.dendrogram
-# #    dend15 %>% set("branches_k_color") %>% plot
-# #    dend15 %>% rect.dendrogram(k=3,
-# #                               border = 8, lty = 5, lwd = 2)
-#
-#
-#    # expect_equal(attr(fixed_dend, "members"), 5)
-# })
-#
-#
-#
+# library(dendextend)
 
 
-# #### this throws an error
-#    dend15 <- c(1:5) %>% dist %>% hclust(method = "average") %>% as.dendrogram
-#    # dend15 <- c(1:25) %>% dist %>% hclust(method = "average") %>% as.dendrogram
-#    dend15 %>% set("branches_k_color") %>% plot
-#    dend15 %>% rect.dendrogram(k=4,
-#                               border = 8, lty = 5, lwd = 2)
+context("rect.dendrogram")
+
+
+test_that("rect.dendrogram works", {
+   
+   hc <- USArrests[1:10, ] %>%
+      dist() %>%
+      hclust() 
+   dend <- as.dendrogram(hc)
+   
+   # standard
+   plot(dend)
+   rect.dendrogram(dend, k = 4, which = 3)
+   rect.dendrogram(dend, k = 4, x = 4)
+   rect.dendrogram(dend, h = 70, x = 1)
+   
+   # horizontal
+   plot(dend, horiz = T)
+   rect.dendrogram(dend, k = 4, which = 3, horiz = T, text = "test")
+   
+   # if non-denrogram object passed in
+   expect_error(
+      rect.dendrogram(hc, k = 4)   
+   )
+   # if more than one k value passed in
+   expect_error(
+      rect.dendrogram(dend, k = c(2,3))   
+   )
+   # if both k and h specified
+   expect_error(
+      rect.dendrogram(dend, k = 2, h = 2)   
+   )
+   # if neither k nor h are specified
+   expect_error(
+      rect.dendrogram(dend)   
+   )
+   # if k == 1
+   expect_error(
+      rect.dendrogram(dend, k = 1, stop_if_out = T)   
+   )
+   expect_warning(
+      rect.dendrogram(dend, k = 1)   
+   )
+   # if both x and which are specified
+   expect_error(
+      rect.dendrogram(dend, k = 4, x = 2, which = 3)   
+   )
+   # if which > k
+   expect_error(
+      rect.dendrogram(dend, k = 4, which = 5)   
+   )
+   # if k >= number of leaves
+   expect_warning(
+      rect.dendrogram(dend, k = 10)
+   )
+   
+})
+
+
+test_that("identify.dendrogram works", {
+   
+   hc <- USArrests[1:10, ] %>%
+      dist() %>%
+      hclust() 
+   dend <- as.dendrogram(hc)
+   
+   # replace locator function with one that automatically clicks to make test non-interactive
+   locator_counter <<- 0
+   with_mock(
+      locator = function(x) {
+         locator_counter <<- locator_counter + 1
+         if (locator_counter == 1) return(list(x = 172, y = 5))
+         if (locator_counter == 2) return(list(x = 172, y = 1))
+         return(NULL)
+      },
+      {
+         plot(dend, horiz = TRUE)
+         vec <- identify(dend, horiz = TRUE, FUN = function(x) x + 1, DEV.FUN = 2)
+      }
+   )
+   expect_identical(
+      vec[[2]], c("Connecticut" = 8)
+   )
+   expect_true(
+      all(vec[[1]] == c(2:7, 9:11))
+   )
+
+})
