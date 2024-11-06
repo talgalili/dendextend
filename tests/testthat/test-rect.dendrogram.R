2d12a55c064044b269a6f8acbb64f1af4b3142b5
alecbuetow <alecbuetow@gmail.com>
2024-11-06 13:32:40 -0500

    added test for rect.dendrogram

--- tests/testthat/test-rect.dendrogram.R
+++ tests/testthat/test-rect.dendrogram.R
@@ -1,26 +1,57 @@
 # library(testthat)
-#
-# context("rect.dendrogram")
-#
-# test_that("rect.dendrogram work",{
-#
-# #    dend15 <- c(1:5) %>% dist %>% hclust(method = "average") %>% as.dendrogram
-# #    # dend15 <- c(1:25) %>% dist %>% hclust(method = "average") %>% as.dendrogram
-# #    dend15 %>% set("branches_k_color") %>% plot
-# #    dend15 %>% rect.dendrogram(k=3,
-# #                               border = 8, lty = 5, lwd = 2)
-#
-#
-#    # expect_equal(attr(fixed_dend, "members"), 5)
-# })
-#
-#
-#
+# library(dendextend)
 
 
-# #### this throws an error
-#    dend15 <- c(1:5) %>% dist %>% hclust(method = "average") %>% as.dendrogram
-#    # dend15 <- c(1:25) %>% dist %>% hclust(method = "average") %>% as.dendrogram
-#    dend15 %>% set("branches_k_color") %>% plot
-#    dend15 %>% rect.dendrogram(k=4,
-#                               border = 8, lty = 5, lwd = 2)
+context("rect.dendrogram")
+
+
+testthat("rect.dendrogram works", {
+   
+   hc <- USArrests[1:10, ] %>%
+      dist() %>%
+      hclust() 
+   dend <- as.dendrogram(hc)
+   
+   # standard
+   plot(dend)
+   rect.dendrogram(dend, k = 4, which = 3)
+   rect.dendrogram(dend, k = 4, x = 4)
+   
+   # horizontal
+   plot(dend, horiz = T)
+   rect.dendrogram(dend, k = 4, which = 3, horiz = T, text = "test")
+   
+   # if non-denrogram object passed in
+   expect_error(
+      rect.dendrogram(hc, k = 4)   
+   )
+   # if more than one k value passed in
+   expect_error(
+      rect.dendrogram(dend, k = c(2,3))   
+   )
+   # if both k and h specified
+   expect_error(
+      rect.dendrogram(dend, k = 2, h = 2)   
+   )
+   # if neither k nor h are specified
+   expect_error(
+      rect.dendrogram(dend)   
+   )
+   # if k == 1
+   expect_error(
+      rect.dendrogram(dend, k = 1, stop_if_out = T)   
+   )
+   expect_warning(
+      rect.dendrogram(dend, k = 1)   
+   )
+   # if both x and which are specified
+   expect_error(
+      rect.dendrogram(dend, k = 4, x = 2, which = 3)   
+   )
+   # if which > k
+   expect_error(
+      rect.dendrogram(dend, k = 4, which = 5)   
+   )
+   
+   207 - 209
+})
