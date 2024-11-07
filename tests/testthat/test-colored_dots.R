bb355955881d14e900284955d53744722b5e80c0
alecbuetow <alecbuetow@gmail.com>
2024-11-07 12:58:37 -0500

    added test for zero_range

---
+++ tests/testthat/test-colored_dots.R
@@ -0,0 +1,39 @@
+# library(testthat)
+# library(dendextend)
+
+
+context("colored_dots")
+
+
+test_that("zero_range works", {
+   
+   x <- c(1)
+   expect_true(
+      zero_range(x)
+   )
+   x <- c(1, 2, 3)
+   expect_error(
+      zero_range(x)
+   )   
+   x <- c(1, NA)
+   expect_true(
+      is.na(zero_range(x))
+   )   
+   x <- c(1, 1)
+   expect_true(
+      zero_range(x)
+   )   
+   x <- c(-Inf, Inf)
+   expect_false(
+      zero_range(x)
+   )   
+   x <- c(0, 1)
+   expect_false(
+      zero_range(x)
+   )   
+   x <- c(1,2)
+   expect_false(
+      zero_range(x)
+   )   
+   
+})
