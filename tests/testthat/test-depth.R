433607d84321ff2278f3e3ac7f96a5f66c3b8e75
alecbuetow <alecbuetow@gmail.com>
2024-11-08 11:03:31 -0500

    added tests for max_depth

--- tests/testthat/test-depth.R
+++ tests/testthat/test-depth.R
@@ -26,3 +26,26 @@ test_that("min_depth works", {
    )
    
 })
+
+
+test_that("max_depth works", {
+   
+   dend <- as.dendrogram(hclust(dist(mtcars)))
+   
+   expect_identical(
+      max_depth(dend),
+      11
+   )
+   
+   # if non-list object passed in
+   x <- 1:4
+   expect_identical(
+      max_depth(x),
+      1L
+   )
+   # if leaf is passed in as a list
+   expect_error(
+      max_depth(as.list(dend[[1]][[1]]))
+   )
+   
+})
