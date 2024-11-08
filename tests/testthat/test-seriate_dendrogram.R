03688f025adab8d97d72fe5eca9e60433686c5f8
alecbuetow <alecbuetow@gmail.com>
2024-11-08 11:40:13 -0500

    added tests for seriate_dendrogram

---
+++ tests/testthat/test-seriate_dendrogram.R
@@ -0,0 +1,51 @@
+# library(testthat)
+# library(dendextend)
+
+
+context("seriate_dendrogram")
+
+
+test_that("seriate_dendrogram works", {
+   suppressWarnings(RNGversion("3.5.0"))
+   set.seed(1)   
+   
+   x <- dist(mtcars)
+   dend <- as.dendrogram(hclust(x))
+   
+   seriated_dend <- seriate_dendrogram(dend, x)
+   expect_identical(
+      labels(seriated_dend)[1:3],
+      c("Honda Civic", "Toyota Corolla", "Fiat 128")   
+   )
+   
+   # if labels in x not identical to labels in dend
+   labels(x)[1] = labels(x)[2]
+   expect_error(
+      seriate_dendrogram(dend, x)
+   )
+   # if x is not a distance object
+   fake_x <- 1:4
+   expect_error(
+      seriate_dendrogram(dend, fake_x)
+   )
+   # if non-dendrogram or non-hclust object passed in
+   fake_dend <- 1:4
+   expect_error(
+      seriate_dendrogram(fake_dend)
+   )
+   # if seriation not installed
+   mock_requireNamespace <- function(package, ...) {
+      if (package == "seriate_dendrogram") return(FALSE)
+      base::requireNamespace(package, ...)
+   }
+   mockery::stub(seriate_dendrogram, "requireNamespace", mock_requireNamespace)
+   expect_error(
+      seriate_dendrogram(dend)
+   )
+   # undo changes that were needed to mock seriation not being installed
+   undo_mock_requireNamespace <- function(package, ...) {
+      base::requireNamespace(package, ...)
+   }
+   mockery::stub(seriate_dendrogram, "requireNamespace", undo_mock_requireNamespace)
+   
+})
