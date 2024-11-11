f5375069d25349c45e6691592e59e345f296aa1c
alecbuetow <alecbuetow@gmail.com>
2024-11-11 13:23:56 -0500

    added tests to prune non-dendrogram objects

--- tests/testthat/test-prune.R
+++ tests/testthat/test-prune.R
@@ -23,3 +23,33 @@ test_that("prune_leaf works", {
    )
    
 })
+
+
+test_that("prune works for non-dendrogram objects", {
+   
+   hc <- USArrests[1:10, ] %>%
+      dist() %>%
+      hclust() 
+   dend <- as.dendrogram(hc)
+   ph <- ape::as.phylo(hc)
+   
+   # should achieve the same result regardless of object type
+   result <- prune.hclust(hc, "Florida")
+   expect_equal(
+      as.dendrogram(result),
+      prune_leaf(dend, "Florida")
+   )
+   result <- prune.phylo(ph, "Florida")
+   expect_equal(
+      as.dendrogram(result),
+      prune_leaf(dend, "Florida")
+   )
+   expect_no_error(
+      prune.rpart(dend, -1000) 
+   )
+   
+   # no default for prune
+   expect_error(
+      prune.default(dend)
+   )
+})
